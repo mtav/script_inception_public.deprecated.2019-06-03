9f7553079ec33ce6795f7ed2bc8fb6f8049299e5
Your Name <Mike.Taverne@bristol.ac.uk>
2010-08-04 13:31:18 +0100

    GEO_INP_reader.m now stores snapshots in a structure :)

--- GEO_INP_reader.m
+++ GEO_INP_reader.m
@@ -1,109 +1,96 @@
 function [ entries, structured_entries ] = GEO_INP_reader(filename)
 
-% ask for input file if not given
-if exist('filename','var')==0
-    disp('filename not given');
-    [file,path] = uigetfile({'*.geo *.inp'},'Select a GEO or INP file');
-    filename=[path,file];
-end
-
-% read the whole file as one string
-fulltext = fileread(filename);
-
-% remove comments
-pattern_stripcomments = '\*\*.*$';
-cleantext =  regexprep(fulltext, pattern_stripcomments, '\n', 'lineanchors', 'dotexceptnewline', 'warnings');
-
-% extract blocks
-pattern_blocks = '^(?<type>\w+).*?\{(?<data>[^\{\}]*?)\}';
-[tokens_blocks match_blocks names_blocks] =  regexp(cleantext, pattern_blocks, 'tokens', 'match', 'names', 'lineanchors', 'warnings');
-
-entries={};
-% process blocks
-for i=1:length(names_blocks)
-
-    type = names_blocks(:,i).type;
-    data = names_blocks(:,i).data;
-%     disp(['===>type=',type]);
-
-    dataV=[];
-    % remove empty lines
-    lines = strread(data,'%s','delimiter','\r');
-    cellFlag=0;
-    for L=1:length(lines)
-        if ~length(lines{L})
-            continue;
-        end
-
-        dd=str2num(lines{L});
-        
-        if cellFlag
-            if length(dd)  %% dd is num
-                dataV{length(dataV)+1}=dd;
-            else           %% dd is not num
-                dataV{length(dataV)+1}=lines{L};
-            end
-        else
-           if length(dd)  %% dd is num
-                dataV=[dataV,dd];
-            else           %% dd is not num
-                cellFlag=1;
-                dataV=num2cell(dataV);
-                dataV{length(dataV)+1}=lines{L};
-            end
-        end
-    end
-    entry.type=type;
-    entry.data=dataV';
-    entries{length(entries)+1}=entry;
-	
-	all_snapshots = struct('hoop', {}, 'target', {})
-	time_snapshots = struct('hoop', {}, 'target', {})
-	frequency_snapshots = struct('hoop', {}, 'target', {})
-	
-	entry.type
-	entry.data(1)
-	switch upper(entry.type)
-		case {'FREQUENCY_SNAPSHOT','SNAPSHOT'}
-			disp('Adding snapshot...');
-			if strcmpi(entry.type,'FREQUENCY_SNAPSHOT')
-			
-			elseif strcmpi(entry.type,'SNAPSHOT')
-			
-			else
-				error('Sense, it makes none.');
-			end
-		otherwise
-			disp('Unknown type.');
+	% ask for input file if not given
+	if exist('filename','var')==0
+		disp('filename not given');
+		[file,path] = uigetfile({'*.geo *.inp'},'Select a GEO or INP file');
+		filename=[path,file];
 	end
-	
-end %end of loop through blocks
 
-structured_entries.all_snapshots = all_snapshots;
-structured_entries.time_snapshots = time_snapshots;
-structured_entries.frequency_snapshots = frequency_snapshots;
+	% read the whole file as one string
+	fulltext = fileread(filename);
 
-	idx = 1;
-	snapshot.first = entry.data(idx);idx = idx+1;
-	snapshot.repetition = entry.data(idx); idx = idx+1;
-	snapshot.plane = entry.data(idx); idx = idx+1;
-	snapshot.P1(1) = entry.data(idx); idx = idx+1;
-	snapshot.P1(2) = entry.data(idx); idx = idx+1;
-	snapshot.P1(3) = entry.data(idx); idx = idx+1;
-	snapshot.P2(1) = entry.data(idx); idx = idx+1;
-	snapshot.P2(2) = entry.data(idx); idx = idx+1;
-	snapshot.P2(3) = entry.data(idx); idx = idx+1;
-	snapshot.E(1) = entry.data(idx); idx = idx+1;
-	snapshot.E(2) = entry.data(idx); idx = idx+1;
-	snapshot.E(3) = entry.data(idx); idx = idx+1;
-	snapshot.H(1) = entry.data(idx); idx = idx+1;
-	snapshot.H(2) = entry.data(idx); idx = idx+1;
-	snapshot.H(3) = entry.data(idx); idx = idx+1;
-	snapshot.J(1) = entry.data(idx); idx = idx+1;
-	snapshot.J(2) = entry.data(idx); idx = idx+1;
-	snapshot.J(3) = entry.data(idx); idx = idx+1;
-	snapshot.power = entry.data(idx); idx = idx+1;
+	% remove comments
+	pattern_stripcomments = '\*\*.*$';
+	cleantext =  regexprep(fulltext, pattern_stripcomments, '\n', 'lineanchors', 'dotexceptnewline', 'warnings');
 
+	% extract blocks
+	pattern_blocks = '^(?<type>\w+).*?\{(?<data>[^\{\}]*?)\}';
+	[tokens_blocks match_blocks names_blocks] =  regexp(cleantext, pattern_blocks, 'tokens', 'match', 'names', 'lineanchors', 'warnings');
+
+	time_snapshots=struct('first',{},'repetition',{},'plane',{},'P1',{},'P2',{},'E',{},'H',{},'J',{},'power',{});
+	frequency_snapshots=struct('first',{},'repetition',{},'interpolate',{},'real_dft',{},'mod_only',{},'mod_all',{},'plane',{},'P1',{},'P2',{},'frequency',{},'starting_sample',{},'E',{},'H',{},'J',{});
+	all_snapshots=struct('first',{},'repetition',{},'interpolate',{},'real_dft',{},'mod_only',{},'mod_all',{},'plane',{},'P1',{},'P2',{},'frequency',{},'starting_sample',{},'E',{},'H',{},'J',{},'power',{});
+
+	entries={};
+	% process blocks
+	for i=1:length(names_blocks)
+
+		type = names_blocks(:,i).type;
+		data = names_blocks(:,i).data;
+		% disp(['===>type=',type]);
+
+		dataV=[];
+		% remove empty lines
+		lines = strread(data,'%s','delimiter','\r');
+		cellFlag=0;
+		for L=1:length(lines)
+			if ~length(lines{L})
+				continue;
+			end
+
+			dd=str2num(lines{L});
+			
+			if cellFlag
+				if length(dd)  %% dd is num
+					dataV{length(dataV)+1}=dd;
+				else           %% dd is not num
+					dataV{length(dataV)+1}=lines{L};
+				end
+			else
+			   if length(dd)  %% dd is num
+					dataV=[dataV,dd];
+				else           %% dd is not num
+					cellFlag=1;
+					dataV=num2cell(dataV);
+					dataV{length(dataV)+1}=lines{L};
+				end
+			end
+		end % end of loop through lines
+		
+		entry.type=type;
+		entry.data=dataV';
+		entries{length(entries)+1}=entry;
+			
+		% entry.type
+		% entry.data(1)
+		switch upper(entry.type)
+			case {'FREQUENCY_SNAPSHOT','SNAPSHOT'}
+				% disp('Adding snapshot...');
+				snapshot = add_snapshot(entry);
+				all_snapshots = [ all_snapshots snapshot ];
+				if strcmpi(entry.type,'FREQUENCY_SNAPSHOT')
+					snapshot = add_frequency_snapshot(entry);
+					frequency_snapshots = [ frequency_snapshots snapshot ];
+				elseif strcmpi(entry.type,'SNAPSHOT')
+					snapshot = add_time_snapshot(entry);
+					time_snapshots = [ time_snapshots snapshot ];
+				else
+					error('Sense, it makes none.');
+				end
+			otherwise
+				% disp('Unknown type.');
+		end % end of switch
+		
+	end %end of loop through blocks
+
+	structured_entries.all_snapshots = all_snapshots;
+	structured_entries.time_snapshots = time_snapshots;
+	structured_entries.frequency_snapshots = frequency_snapshots;
+
+end % end of function
+
+function snapshot = add_frequency_snapshot(entry)
 	idx = 1;
 	snapshot.first = entry.data(idx); idx = idx+1;
 	snapshot.repetition = entry.data(idx); idx = idx+1;
@@ -112,20 +99,64 @@ structured_entries.frequency_snapshots = frequency_snapshots;
 	snapshot.mod_only = entry.data(idx); idx = idx+1;
 	snapshot.mod_all = entry.data(idx); idx = idx+1;
 	snapshot.plane = entry.data(idx); idx = idx+1;
-	snapshot.P1(1) = entry.data(idx); idx = idx+1;
-	snapshot.P1(2) = entry.data(idx); idx = idx+1;
-	snapshot.P1(3) = entry.data(idx); idx = idx+1;
-	snapshot.P2(1) = entry.data(idx); idx = idx+1;
-	snapshot.P2(2) = entry.data(idx); idx = idx+1;
-	snapshot.P2(3) = entry.data(idx); idx = idx+1;
+	snapshot.P1 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.P2 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
 	snapshot.frequency = entry.data(idx); idx = idx+1;
 	snapshot.starting_sample = entry.data(idx); idx = idx+1;
-	snapshot.E(1) = entry.data(idx); idx = idx+1;
-	snapshot.E(2) = entry.data(idx); idx = idx+1;
-	snapshot.E(3) = entry.data(idx); idx = idx+1;
-	snapshot.H(1) = entry.data(idx); idx = idx+1;
-	snapshot.H(2) = entry.data(idx); idx = idx+1;
-	snapshot.H(3) = entry.data(idx); idx = idx+1;
-	snapshot.J(1) = entry.data(idx); idx = idx+1;
-	snapshot.J(2) = entry.data(idx); idx = idx+1;
-	snapshot.J(3) = entry.data(idx); idx = idx+1;
+	snapshot.E = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.H = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.J = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+end
+
+function snapshot = add_time_snapshot(entry)
+	idx = 1;
+	snapshot.first = entry.data(idx); idx = idx+1;
+	snapshot.repetition = entry.data(idx); idx = idx+1;
+	snapshot.plane = entry.data(idx); idx = idx+1;
+	snapshot.P1 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.P2 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.E = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.H = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.J = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+	snapshot.power = entry.data(idx); idx = idx+1;
+end
+
+function snapshot = add_snapshot(entry)
+	if strcmpi(entry.type,'FREQUENCY_SNAPSHOT')
+		idx = 1;
+		snapshot.first = entry.data(idx); idx = idx+1;
+		snapshot.repetition = entry.data(idx); idx = idx+1;
+		snapshot.interpolate = entry.data(idx); idx = idx+1;
+		snapshot.real_dft = entry.data(idx); idx = idx+1;
+		snapshot.mod_only = entry.data(idx); idx = idx+1;
+		snapshot.mod_all = entry.data(idx); idx = idx+1;
+		snapshot.plane = entry.data(idx); idx = idx+1;
+		snapshot.P1 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.P2 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.frequency = entry.data(idx); idx = idx+1;
+		snapshot.starting_sample = entry.data(idx); idx = idx+1;
+		snapshot.E = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.H = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.J = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.power = -1;
+	elseif strcmpi(entry.type,'SNAPSHOT')
+		idx = 1;
+		snapshot.first = entry.data(idx); idx = idx+1;
+		snapshot.repetition = entry.data(idx); idx = idx+1;
+		snapshot.interpolate = -1;
+		snapshot.real_dft = -1;
+		snapshot.mod_only = -1;
+		snapshot.mod_all = -1;
+		snapshot.plane = entry.data(idx); idx = idx+1;
+		snapshot.P1 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.P2 = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.frequency = -1;
+		snapshot.starting_sample = -1;
+		snapshot.E = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.H = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.J = [entry.data(idx), entry.data(idx+1), entry.data(idx+2)]; idx = idx+3;
+		snapshot.power = entry.data(idx); idx = idx+1;
+	else
+		error('Sense, it makes none.');
+	end
+end
