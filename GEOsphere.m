992027071399832bd2765722c87054db1fb29a35
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2010-08-12 15:48:07 +0100

    adding GEOsphere + yagi_dome

---
+++ GEOsphere.m
@@ -0,0 +1,19 @@
+function GEOsphere(FILE, center, inner_radius, outer_radius, permittivity, conductivity)
+	% sphere
+	% {
+	% 1-5 Coordinates of the sphere ( xc yc zc r1 r2 )
+	% 6 permittivity
+	% 7 conductivity
+	% }
+	fprintf(FILE,'SPHERE  **SPHERE DEFINITION\n');
+	fprintf(FILE,'{\n');
+	fprintf(FILE,'%E **XC\n', center(1));
+	fprintf(FILE,'%E **YC\n', center(2));
+	fprintf(FILE,'%E **ZC\n', center(3));
+	fprintf(FILE,'%E **R1\n', inner_radius);
+	fprintf(FILE,'%E **R2\n', outer_radius);
+	fprintf(FILE,'%E **permittivity\n', permittivity);
+	fprintf(FILE,'%E **conductivity\n', conductivity);
+	fprintf(FILE,'}\n');
+	fprintf(FILE,'\n');
+end
