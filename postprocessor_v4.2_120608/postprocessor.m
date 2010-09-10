9c17ae148b2edd5e157053fcfc3d4f8d75deb736
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2010-09-10 11:19:38 +0100

    CLI passing of dir OK

--- postprocessor_v4.2_120608/postprocessor.m
+++ postprocessor_v4.2_120608/postprocessor.m
@@ -51,54 +51,48 @@ end
 
 % --- Executes just before postprocessor is made visible.
 function postprocessor_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to postprocessor (see VARARGIN)
+	% This function has no output args, see OutputFcn.
+	% hObject    handle to figure
+	% eventdata  reserved - to be defined in a future version of MATLAB
+	% handles    structure with handles and user data (see GUIDATA)
+	% varargin   command line arguments to postprocessor (see VARARGIN)
 
-disp(['nargin=',num2str(nargin)]);
-disp(['nargout=',num2str(nargout)]);
+	disp(['nargin=',num2str(nargin)]);
+	disp(['nargout=',num2str(nargout)]);
 
-if nargin>4
-    disp('We have input');
-end
+	if nargin>4
+		disp('We have input');
+	end
 
-% set default value
-handles.workdir = pwd();
+	% set default value
+	new_dir = pwd();
 
-% CLI input arg handling
-if nargin > 3
-    for k = 1:length(varargin)
-        disp(varargin{k});
-    end
+	% CLI input arg handling
+	if nargin > 3
+		for k = 1:length(varargin)
+			disp(varargin{k});
+		end
 
-    if exist(varargin{1}{1},'dir')
-        handles.workdir = varargin{1}{1};
-    else
-        errordlg({'Input argument must be a valid',...
-                 'folder'},'Input Argument Error!')
-        return
-    end
-end
+		if exist(varargin{1}{1},'dir')
+			new_dir = varargin{1}{1};
+		else
+			errordlg({'Input argument must be a valid',...
+					 'folder'},'Input Argument Error!')
+			return
+		end
+	end
 
-handles.toto='original text';
-disp(['before:',handles.toto]);
-[handles] = setWorkDir(hObject, handles, 'new text');
-disp(['after:',handles.toto]);
+	[handles] = setWorkDir(handles, new_dir);
 
-% Choose default command line output for postprocessor
-handles.output = hObject;
+	% Choose default command line output for postprocessor
+	handles.output = hObject;
 
-% Update handles structure
-set(handles.label_working_directory,'String',handles.workdir)
-guidata(hObject, handles);
+	% Update handles structure
+	% set(handles.label_working_directory,'String',handles.workdir)
+	guidata(hObject, handles);
 
-% setWorkDir(hObject, handles, 'new text');
-
-% UIWAIT makes postprocessor wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
+	% UIWAIT makes postprocessor wait for user response (see UIRESUME)
+	% uiwait(handles.figure1);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = postprocessor_OutputFcn(hObject, eventdata, handles) 
@@ -311,15 +305,36 @@ function edit3_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of edit3 as text
 %        str2double(get(hObject,'String')) returns contents of edit3 as a double
 
-function [handles] = setWorkDir(hObject,handles, str)
-    disp(handles.toto);
-    disp(str);    
-    handles.toto=str;
-    disp(handles.toto);
-    guidata(hObject, handles);
-    disp(handles.toto);
-    set(handles.label_working_directory,'String',str)
+function [handles] = setWorkDir(handles, new_dir)
 
+	handles.workdir = new_dir;
+
+	set(handles.label_working_directory,'String',new_dir);
+
+	handles.snaplist = {};
+	handles.geolist = {};
+	handles.inplist = {};
+
+	prn_files = dir(fullfile(new_dir,'*.prn'));
+	handles.snaplist = {prn_files.name}';
+	prn_files = char(prn_files.name);
+	geo_files = dir(fullfile(new_dir,'*.geo'));
+	handles.geolist = {geo_files.name}';
+	geo_files = char(geo_files.name);
+	inp_files = dir(fullfile(new_dir,'*.inp'));
+	handles.inplist = {inp_files.name}';
+	inp_files = char(inp_files.name);
+
+	% disp(['prn_files=',prn_files]);
+	% if(prn_files=='')
+	%     disp('no .prn files found');
+	% end
+
+	set(handles.popupmenu_inputsnapshot,'String',prn_files);
+	set(handles.popupmenu_geometryfile,'String',geo_files);
+	set(handles.popupmenu_inputfile,'String',inp_files);
+
+	clear prn_files geo_files inp_files
 
 % --- Executes during object creation, after setting all properties.
 function edit3_CreateFcn(hObject, eventdata, handles)
@@ -344,36 +359,8 @@ if new_dir == 0
     return
 end
 
-handles.workdir = new_dir;
+[handles] = setWorkDir(handles, new_dir);
 
-set(handles.label_working_directory,'String',new_dir);
-
-handles.snaplist = {};
-handles.geolist = {};
-handles.inplist = {};
-
-prn_files = dir(fullfile(new_dir,'*.prn'));
-handles.snaplist = {prn_files.name}';
-prn_files = char(prn_files.name);
-geo_files = dir(fullfile(new_dir,'*.geo'));
-handles.geolist = {geo_files.name}';
-geo_files = char(geo_files.name);
-inp_files = dir(fullfile(new_dir,'*.inp'));
-handles.inplist = {inp_files.name}';
-inp_files = char(inp_files.name);
-
-% disp(['prn_files=',prn_files]);
-% if(prn_files=='')
-%     disp('no .prn files found');
-% end
-
-set(handles.popupmenu_inputsnapshot,'String',prn_files);
-set(handles.popupmenu_geometryfile,'String',geo_files);
-set(handles.popupmenu_inputfile,'String',inp_files);
-
-clear prn_files geo_files inp_files
-
-handles.workdir = new_dir;
 guidata(hObject,handles);
 
 function edit4_Callback(hObject, eventdata, handles)
