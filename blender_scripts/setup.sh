d9a2dd714cc42818ced8b6cf0d931767b47b3b34
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2010-10-13 12:59:16 +0100

    added setup.sh script for easier install/update

---
+++ blender_scripts/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -eux
+
+BLENDERSCRIPTDIR="/c/Documents and Settings/eempct/Application Data/Blender Foundation/Blender/.blender/scripts"
+SCRIPTS="bfdtd_import.py bfdtd_parser.py"
+
+function BlenderScriptDir_to_repo()
+{
+    echo "BlenderScriptDir->repo"
+    for f in $SCRIPTS
+    do
+        cp -iv "$BLENDERSCRIPTDIR/$f" ".";
+    done
+}
+
+function repo_to_BlenderScriptDir()
+{
+    echo "repo->BlenderScriptDir";
+    for f in $SCRIPTS
+    do
+        cp -iv "$f" "$BLENDERSCRIPTDIR";
+    done
+}
+
+echo "0=BlenderScriptDir->repo / 1=repo->BlenderScriptDir"
+read ans
+case $ans in
+  0) BlenderScriptDir_to_repo;;
+  1) repo_to_BlenderScriptDir;;
+  *) echo "Unknown option";;
+esac
+# echo "BLENDERSCRIPTDIR";
+# echo "BLENDERSCRIPTDIR=$BLENDERSCRIPTDIR";
+# ls "$BLENDERSCRIPTDIR";
