166500598e35003b8fa782ed710b2190cf80063a
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2010-12-03 17:11:23 +0000

    matlab path stuff

--- getuserdir.m
+++ getuserdir.m
@@ -1,16 +1,17 @@
 function userDir = getuserdir()
-%GETUSERDIR   return the user home directory.
-%   USERDIR = GETUSERDIR returns the user home directory using the registry
-%   on windows systems and using Java on non windows systems as a string
-%
-%   Example:
-%      getuserdir() returns on windows
-%           C:\Documents and Settings\MyName\Eigene Dateien
+    %GETUSERDIR   return the user home directory.
+    %   USERDIR = GETUSERDIR returns the user home directory using the registry
+    %   on windows systems and using Java on non windows systems as a string
+    %
+    %   Example:
+    %      getuserdir() returns on windows
+    %           C:\Documents and Settings\MyName\Eigene Dateien
 
-if ispc
-    userDir = winqueryreg('HKEY_CURRENT_USER',...
-        ['Software\Microsoft\Windows\CurrentVersion\' ...
-         'Explorer\Shell Folders'],'Personal');
-else
-    userDir = char(java.lang.System.getProperty('user.home'));
+    if ispc
+        userDir = winqueryreg('HKEY_CURRENT_USER',...
+            ['Software\Microsoft\Windows\CurrentVersion\' ...
+             'Explorer\Shell Folders'],'Personal');
+    else
+        userDir = char(java.lang.System.getProperty('user.home'));
+    end
 end
