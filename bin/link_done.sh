9e20e7f63fbf83b7f58459e1dd8429b84d8f78a6
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2010-12-13 18:24:24 +0000

    added usgae instructions

--- bin/link_done.sh
+++ bin/link_done.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eu
 
+if [ $# -lt 2 ]
+then
+        echo "usage :"
+        echo "`basename $0` DEST  file1.out file2.out ..."
+	echo "creates links to the dirs containing *.out in DEST if *.out contains \"Deallocating\", i.e. if the simulations in those dirs are finished"
+        exit 0
+fi
+
 DST=$(readlink -f $1)
 shift
 for f in "$@"
