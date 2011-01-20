8bc773fa8c89ca52de0db47e4cbb6e0ce2494e15
Erman <ermanengin@gmail.com>
2011-01-20 18:18:41 +0000

    ermans test

---
+++ utilities/closestInd.m
@@ -0,0 +1,11 @@
+% Finds the index of the closest point of a given vector or matrix to the
+% given point.
+
+function [ind,val]=closestInd(M,p)
+N=abs(M-p);
+err=N;
+for m=1:ndims(N)
+    err=min(err);
+end
+ind=find(N==err);
+val=M(ind);
