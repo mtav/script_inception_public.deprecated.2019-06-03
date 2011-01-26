46cd334d593ddd92fb43d6dd74437402b891d613
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-01-26 18:53:22 +0000

    added option to list unfinished sims in link_done.sh

--- bin/link_done.sh
+++ bin/link_done.sh
@@ -4,8 +4,9 @@ set -eu
 usage()
 {
         echo "usage :"
-        echo "`basename $0` 0 file1.out file2.out ..."
-        echo "`basename $0` 1 DEST  file1.out file2.out ..."
+        echo "`basename $0` 0 file1.out file2.out ... (just list finished ones)"
+        echo "`basename $0` 1 DEST  file1.out file2.out ... (create links to finished ones in DEST)"
+        echo "`basename $0` 2 file1.out file2.out ... (list unfinished ones)"
 	echo "creates links to the dirs containing *.out in DEST if *.out contains \"Deallocating\", i.e. if the simulations in those dirs are finished"
         exit 0
 }
@@ -46,6 +47,17 @@ then
 				echo "ln -s $DIR $LINKNAME"
 				ln -s $DIR $LINKNAME
 			fi
+		fi
+	done
+elif [ $justlink = "2" ]
+then
+	for f in "$@"
+	do
+		DIR=$(dirname $(readlink -f $f))
+		BASE=$(basename $DIR)
+		if ! grep Deallocating  $f 1>/dev/null 2>&1
+		then
+			echo "$f"
 		fi
 	done
 else
