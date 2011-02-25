62b184415c63b056ee2955ba27eae11120108579
Mike Taverne <mtaverne@bluemarlin.engits.com>
2011-02-25 19:18:16 +0000

    fixed qdel_all

--- bin/qdel_all.sh
+++ bin/qdel_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
 # alias coming soon. I want dropbox sync first. :)
-qstat | grep $USER | awk '{print $1}' | xargs qdel
+qstat | grep $USER | awk -F. '{print $1}' | xargs qdel
 
