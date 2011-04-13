19deff88076ee2f8c68143dd42e20c64fae5ad82
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-04-13 19:19:00 +0100

    postprocessor_CLI basically OK

--- postprocessor/plotgen.m
+++ postprocessor/plotgen.m
@@ -1,224 +1,225 @@
 function plotgen(maxval,column,handles)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Function to display results from frequency snapshots and poynting
-%vector calculations from University of Bristol FDTD software
-%
-%Written by Ian Buss 2006
-%
-%Must be used in conjunction with companion files: readtextfile.m,
-%hdrload.m
-%
-%Version 4.2
-%
-%For Poynting vector plots in the same format use snap_poy_int.m
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Load geo file data
-Geoparms = geometryparms(handles);
-
-%% Load input file data
-Inpparms = inputparms(handles);
-
-%% Determine size of snapshot
-ii=1; ValPrev = handles.fin1(ii,1); grid_j = 1;
-while ii<handles.gr(1)
-    if handles.fin1(ii,1) ~= ValPrev
-        ValPrev = handles.fin1(ii,1);
-        grid_j = grid_j+1;
-    end
-    ii=ii+1;
-end
-grid_i = handles.gr(1)/grid_j;
-
-%% Create meshgrids for snapshot
-for pp=1:grid_j
-    for qq=1:grid_i
-        i(pp,qq)=handles.fin1(qq,2);
-    end
-end
-
-for pp=1:grid_j
-    for qq=1:grid_i
-        j(pp,qq)=handles.fin1((qq+((pp-1)*grid_i)),1);
-    end
-end
-
-%% Load column of choice to data
-modu = 1;
-if get(handles.checkbox_modulus,'Value') == 1
-    data = abs(handles.fin1(:,column));
-else
-    data = handles.fin1(:,column);
-    if min(data) < 0
-        modu = 0;
-    end
-end
-
-%% Create plot data meshgrid
-count=1;
-for ii=1:grid_j
-    kk=grid_i*count;
-    for jj=1:grid_i
-        k(ii,jj)=data(jj+(kk-grid_i));
-    end
-    count=count+1;
-end
-
-%% Create figure and plot data
-figure
-grey = 0;
-if (get(handles.radiobutton_colour,'Value') > get(handles.radiobutton_greyscale,'Value'))
-    colormap(jet(256));
-    grey = 0;
-else
-    colormap(gray(256));
-    grey = 1;
-end
-
-if handles.plane == 1
-    if (get(handles.radiobutton_surface,'Value') > get(handles.radiobutton_contour,'Value')) 
-        %pcolor(i,j,k)
-        surf(i,j,k)
-    else
-        contour(i,j,k)
-    end
-elseif handles.plane == 2
-    if (get(handles.radiobutton_surface,'Value') > get(handles.radiobutton_contour,'Value')) 
-        %pcolor(i,j,k)
-        surf(i,j,k)
-    else
-        contour(i,j,k)
-    end
-else
-    if (get(handles.radiobutton_surface,'Value') > get(handles.radiobutton_contour,'Value')) 
-        %pcolor(j,i,k)
-        surf(j,i,k)
-    else
-        j
-        i
-        k
-        contour(j,i,k)
-    end
-end
-
-%colave = max(fin1(:,column));
-colfig = handles.colplot{column};
-if modu == 1 || get(handles.checkbox_modulus,'Value') == 1
-    disp(['maxval=',num2str(maxval)]);
-    caxis([0 maxval])
-else
-    caxis([-maxval maxval])
-end
-colorbar
-AspectRatio = get(gca,'DataAspectRatio');
-AspectRatio(1) = AspectRatio(2);
-set(gca,'DataAspectRatio',AspectRatio);
-if get(handles.checkbox_interpolate,'Value') == 1
-    shading interp
-else
-    shading flat
-end
-switch handles.plane
-    case 1
-        xlabel('z')
-        ylabel('y')
-        axis([Geoparms.Box.Zl Geoparms.Box.Zu Geoparms.Box.Yl Geoparms.Box.Yu])
-    case 2
-        xlabel('z')
-        ylabel('x')
-        axis([Geoparms.Box.Zl Geoparms.Box.Zu Geoparms.Box.Xl Geoparms.Box.Xu])
-    case 3
-        xlabel('x')
-        ylabel('y')
-        axis([Geoparms.Box.Xl Geoparms.Box.Xu Geoparms.Box.Yl Geoparms.Box.Yu])
-end
-titlesnap = strread(handles.snapfile,'%s','delimiter','\\');
-title([char(titlesnap(length(titlesnap))) ': ' char(handles.colplot(column))],'FontWeight','bold');
-clear titlesnap
-hold on
-
-%% Plot Geometry Entities
-if get(handles.checkbox_geometry,'Value') == 1
-    disp('DRAWING GEOMETRY')
-    switch handles.plane
-        case 1
-            %Blocks
-            if Geoparms.Blks > 0
-                for ii=1:1:length(Geoparms.Block)
-                    plot([Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zl],...
-                    [Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yl],'y','LineWidth',2);
-                end
-            end
-            if Geoparms.Cyls > 0
-                for ii=1:1:length(Geoparms.Cylinder)
-                    I = [(Geoparms.Cylinder(1,ii).Z-Geoparms.Cylinder(1,ii).Rad1) ...
-                        (Geoparms.Cylinder(1,ii).Z-Geoparms.Cylinder(1,ii).Rad2) ...
-                        (Geoparms.Cylinder(1,ii).Z+Geoparms.Cylinder(1,ii).Rad2) ...
-                        (Geoparms.Cylinder(1,ii).Z+Geoparms.Cylinder(1,ii).Rad1) ...
-                        (Geoparms.Cylinder(1,ii).Z-Geoparms.Cylinder(1,ii).Rad1)];
-                    J = [(Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2))];
-                    plot(I,J,'y','LineWidth',2);
-                    clear I J
-                end
-            end
-        case 2
-            %Blocks
-            if Geoparms.Blks > 0
-                for ii=1:1:length(Geoparms.Block)
-                    plot([Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zl],...
-                   [Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xl],'y','LineWidth',2);
-                end
-            end
-            if Geoparms.Cyls > 0
-                t = 0:0.1:((2*pi)+0.1);
-                circle_i = cos(t);
-                circle_j = sin(t);
-                for ii=1:1:length(Geoparms.Cylinder)
-                    I = (Geoparms.Cylinder(1,ii).Rad1*circle_i)+Geoparms.Cylinder(1,ii).X;
-                    J = (Geoparms.Cylinder(1,ii).Rad1*circle_j)+Geoparms.Cylinder(1,ii).Z;
-                    plot(J,I,'y'); clear I J
-                    I = (Geoparms.Cylinder(1,ii).Rad2*circle_i)+Geoparms.Cylinder(1,ii).X;
-                    J = (Geoparms.Cylinder(1,ii).Rad2*circle_j)+Geoparms.Cylinder(1,ii).Z;
-                    plot(J,I,'y','LineWidth',2); clear I J
-                end
-            end
-        case 3
-            %Blocks
-            if Geoparms.Blks > 0
-                for ii=1:1:length(Geoparms.Block)
-                    plot([Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Zl],...
-                   [Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yl],'y','LineWidth',2);
-                end
-            end
-            if Geoparms.Cyls > 0
-                for ii=1:1:length(Geoparms.Cylinder)
-                    I = [(Geoparms.Cylinder(1,ii).X-Geoparms.Cylinder(1,ii).Rad1) ...
-                        (Geoparms.Cylinder(1,ii).X-Geoparms.Cylinder(1,ii).Rad2) ...
-                        (Geoparms.Cylinder(1,ii).X+Geoparms.Cylinder(1,ii).Rad2) ...
-                        (Geoparms.Cylinder(1,ii).X+Geoparms.Cylinder(1,ii).Rad1) ...
-                        (Geoparms.Cylinder(1,ii).X-Geoparms.Cylinder(1,ii).Rad1)];
-                    J = [(Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
-                        (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2))];
-                    plot(I,J,'y','LineWidth',2);
-                    clear I J
-                end
-            end
-    end
-end
-
-if get(handles.checkbox_autosave,'Value') == 1
-    dim = length(handles.snapfile);
-    if grey == 1
-        figout = [handles.snapfile(1:(dim-4)) '_' colfig '_' num2str(maxval) '_grey.png'];
-    else
-        figout = [handles.snapfile(1:(dim-4)) '_' colfig '_' num2str(maxval) '.png'];
-    end
-    print(gcf,'-dpng','-r300',figout)
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %Function to display results from frequency snapshots and poynting
+  %vector calculations from University of Bristol FDTD software
+  %
+  %Written by Ian Buss 2006
+  %
+  %Must be used in conjunction with companion files: readtextfile.m,
+  %hdrload.m
+  %
+  %Version 4.2
+  %
+  %For Poynting vector plots in the same format use snap_poy_int.m
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  %% Load geo file data
+  Geoparms = geometryparms(handles);
+  
+  %% Load input file data
+  Inpparms = inputparms(handles);
+  
+  %% Determine size of snapshot
+  ii=1; ValPrev = handles.fin1(ii,1); grid_j = 1;
+  while ii<handles.gr(1)
+      if handles.fin1(ii,1) ~= ValPrev
+          ValPrev = handles.fin1(ii,1);
+          grid_j = grid_j+1;
+      end
+      ii=ii+1;
+  end
+  grid_i = handles.gr(1)/grid_j;
+  
+  %% Create meshgrids for snapshot
+  for pp=1:grid_j
+      for qq=1:grid_i
+          i(pp,qq)=handles.fin1(qq,2);
+      end
+  end
+  
+  for pp=1:grid_j
+      for qq=1:grid_i
+          j(pp,qq)=handles.fin1((qq+((pp-1)*grid_i)),1);
+      end
+  end
+  
+  %% Load column of choice to data
+  modu = 1;
+  if get(handles.checkbox_modulus,'Value') == 1
+      data = abs(handles.fin1(:,column));
+  else
+      data = handles.fin1(:,column);
+      if min(data) < 0
+          modu = 0;
+      end
+  end
+  
+  %% Create plot data meshgrid
+  count=1;
+  for ii=1:grid_j
+      kk=grid_i*count;
+      for jj=1:grid_i
+          k(ii,jj)=data(jj+(kk-grid_i));
+      end
+      count=count+1;
+  end
+  
+  %% Create figure and plot data
+  figure
+  grey = 0;
+  if (get(handles.radiobutton_colour,'Value') > get(handles.radiobutton_greyscale,'Value'))
+      colormap(jet(256));
+      grey = 0;
+  else
+      colormap(gray(256));
+      grey = 1;
+  end
+  
+  if handles.plane == 1
+      if (get(handles.radiobutton_surface,'Value') > get(handles.radiobutton_contour,'Value')) 
+          %pcolor(i,j,k)
+          surf(i,j,k)
+      else
+          contour(i,j,k)
+      end
+  elseif handles.plane == 2
+      if (get(handles.radiobutton_surface,'Value') > get(handles.radiobutton_contour,'Value')) 
+          %pcolor(i,j,k)
+          surf(i,j,k)
+      else
+          contour(i,j,k)
+      end
+  else
+      if (get(handles.radiobutton_surface,'Value') > get(handles.radiobutton_contour,'Value')) 
+          %pcolor(j,i,k)
+          surf(j,i,k)
+      else
+          j
+          i
+          k
+          contour(j,i,k)
+      end
+  end
+  
+  %colave = max(fin1(:,column));
+  colfig = handles.colplot{column};
+  if modu == 1 || get(handles.checkbox_modulus,'Value') == 1
+      disp(['maxval=',num2str(maxval)]);
+      caxis([0 maxval])
+  else
+      caxis([-maxval maxval])
+  end
+  colorbar
+  AspectRatio = get(gca,'DataAspectRatio');
+  AspectRatio(1) = AspectRatio(2);
+  set(gca,'DataAspectRatio',AspectRatio);
+  if get(handles.checkbox_interpolate,'Value') == 1
+      shading interp
+  else
+      shading flat
+  end
+  switch handles.plane
+      case 1
+          xlabel('z')
+          ylabel('y')
+          axis([Geoparms.Box.Zl Geoparms.Box.Zu Geoparms.Box.Yl Geoparms.Box.Yu])
+      case 2
+          xlabel('z')
+          ylabel('x')
+          axis([Geoparms.Box.Zl Geoparms.Box.Zu Geoparms.Box.Xl Geoparms.Box.Xu])
+      case 3
+          xlabel('x')
+          ylabel('y')
+          axis([Geoparms.Box.Xl Geoparms.Box.Xu Geoparms.Box.Yl Geoparms.Box.Yu])
+  end
+  titlesnap = strread(handles.snapfile,'%s','delimiter','\\');
+  title([char(titlesnap(length(titlesnap))) ': ' char(handles.colplot(column))],'FontWeight','bold');
+  clear titlesnap
+  hold on
+  
+  %% Plot Geometry Entities
+  if get(handles.checkbox_geometry,'Value') == 1
+      disp('DRAWING GEOMETRY')
+      switch handles.plane
+          case 1
+              %Blocks
+              if Geoparms.Blks > 0
+                  for ii=1:1:length(Geoparms.Block)
+                      plot([Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zl],...
+                      [Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yl],'y','LineWidth',2);
+                  end
+              end
+              if Geoparms.Cyls > 0
+                  for ii=1:1:length(Geoparms.Cylinder)
+                      I = [(Geoparms.Cylinder(1,ii).Z-Geoparms.Cylinder(1,ii).Rad1) ...
+                          (Geoparms.Cylinder(1,ii).Z-Geoparms.Cylinder(1,ii).Rad2) ...
+                          (Geoparms.Cylinder(1,ii).Z+Geoparms.Cylinder(1,ii).Rad2) ...
+                          (Geoparms.Cylinder(1,ii).Z+Geoparms.Cylinder(1,ii).Rad1) ...
+                          (Geoparms.Cylinder(1,ii).Z-Geoparms.Cylinder(1,ii).Rad1)];
+                      J = [(Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2))];
+                      plot(I,J,'y','LineWidth',2);
+                      clear I J
+                  end
+              end
+          case 2
+              %Blocks
+              if Geoparms.Blks > 0
+                  for ii=1:1:length(Geoparms.Block)
+                      plot([Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zl Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zu Geoparms.Block(1,ii).Zl],...
+                     [Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xl],'y','LineWidth',2);
+                  end
+              end
+              if Geoparms.Cyls > 0
+                  t = 0:0.1:((2*pi)+0.1);
+                  circle_i = cos(t);
+                  circle_j = sin(t);
+                  for ii=1:1:length(Geoparms.Cylinder)
+                      I = (Geoparms.Cylinder(1,ii).Rad1*circle_i)+Geoparms.Cylinder(1,ii).X;
+                      J = (Geoparms.Cylinder(1,ii).Rad1*circle_j)+Geoparms.Cylinder(1,ii).Z;
+                      plot(J,I,'y'); clear I J
+                      I = (Geoparms.Cylinder(1,ii).Rad2*circle_i)+Geoparms.Cylinder(1,ii).X;
+                      J = (Geoparms.Cylinder(1,ii).Rad2*circle_j)+Geoparms.Cylinder(1,ii).Z;
+                      plot(J,I,'y','LineWidth',2); clear I J
+                  end
+              end
+          case 3
+              %Blocks
+              if Geoparms.Blks > 0
+                  for ii=1:1:length(Geoparms.Block)
+                      plot([Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xl Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Xu Geoparms.Block(1,ii).Zl],...
+                     [Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yu Geoparms.Block(1,ii).Yl Geoparms.Block(1,ii).Yl],'y','LineWidth',2);
+                  end
+              end
+              if Geoparms.Cyls > 0
+                  for ii=1:1:length(Geoparms.Cylinder)
+                      I = [(Geoparms.Cylinder(1,ii).X-Geoparms.Cylinder(1,ii).Rad1) ...
+                          (Geoparms.Cylinder(1,ii).X-Geoparms.Cylinder(1,ii).Rad2) ...
+                          (Geoparms.Cylinder(1,ii).X+Geoparms.Cylinder(1,ii).Rad2) ...
+                          (Geoparms.Cylinder(1,ii).X+Geoparms.Cylinder(1,ii).Rad1) ...
+                          (Geoparms.Cylinder(1,ii).X-Geoparms.Cylinder(1,ii).Rad1)];
+                      J = [(Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y+(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2)) ...
+                          (Geoparms.Cylinder(1,ii).Y-(Geoparms.Cylinder(1,ii).H/2))];
+                      plot(I,J,'y','LineWidth',2);
+                      clear I J
+                  end
+              end
+      end
+  end
+  
+  if get(handles.checkbox_autosave,'Value') == 1
+      dim = length(handles.snapfile);
+      if grey == 1
+          figout = [handles.snapfile(1:(dim-4)) '_' colfig '_' num2str(maxval) '_grey.png'];
+      else
+          figout = [handles.snapfile(1:(dim-4)) '_' colfig '_' num2str(maxval) '.png'];
+      end
+      print(gcf,'-dpng','-r300',figout)
+  end
 end
--- postprocessor/postprocessor_CLI.m
+++ postprocessor/postprocessor_CLI.m
@@ -6,12 +6,22 @@ function postprocessor_CLI()
   handles.geolist = {};
   handles.inplist = {};
   [handles, dirChosen] = browse(handles);
-  if dirChosen;
-    handles.inputsnapshot = 1;
-    handles.geometryfile = 1;
-    handles.inputfile = 1;
-    handles = load_data(handles);
+  if ~dirChosen
+    return
   end
+  handles.Type = 1;
+  handles.ProbeID = 1;
+  handles.TimeSnapshotID = 1;
+  handles.FrequencySnapshotID = 1;
+  handles.geometryfile = 1;
+  handles.inputfile = 1;
+  [ handles, isLoaded ] = load_data(handles);
+  if ~isLoaded
+    return
+  end
+  handles.col = 1;
+  handles.maxplotvalue = 1;
+  handles = generate_plot(handles);
 end
 
 function [handles, dirChosen] = browse(handles)
@@ -34,13 +44,30 @@ function handles = setWorkDir(handles, new_dir)
   disp('function [handles] = setWorkDir(handles, new_dir)')
   handles.workdir = new_dir;
 
-  handles.snaplist = {};
+  handles.data_files = {};
+  handles.ProbeList = {};
+  handles.TimeSnapshotList = {};
+  handles.FrequencySnapshotList = {};
   handles.geolist = {};
   handles.inplist = {};
 
-  prn_files = [dir(fullfile(new_dir,'*.prn')); dir(fullfile(new_dir,'*.dat'))];
-  handles.snaplist = {prn_files.name}; handles.snaplist = handles.snaplist';
-  prn_files = char(prn_files.name);
+  data_files = [dir(fullfile(new_dir,'*.prn')); dir(fullfile(new_dir,'*.dat'))];
+  handles.data_files = {data_files.name}; handles.data_files = handles.data_files';
+  for idx=1:length(handles.data_files)
+    if ~isempty(regexp(handles.data_files{idx},'^p.*id\.(prn|dat)$','ignorecase'))
+      handles.ProbeList{end+1} = handles.data_files{idx};
+    end
+    if ~isempty(regexp(handles.data_files{idx},'^[xyz]\d+id\d\d\.(prn|dat)$','ignorecase'))
+      handles.TimeSnapshotList{end+1} = handles.data_files{idx};
+    end
+    if ~isempty(regexp(handles.data_files{idx},'^[xyz]ABid\d\d\.(prn|dat)$','ignorecase'))
+      handles.FrequencySnapshotList{end+1} = handles.data_files{idx};
+    end
+  end
+ 
+  %prn_files = [dir(fullfile(new_dir,'*.prn')); dir(fullfile(new_dir,'*.dat'))];
+  %handles.snaplist = {prn_files.name}; handles.snaplist = handles.snaplist';
+  %prn_files = char(prn_files.name);
     
   geo_files = dir(fullfile(new_dir,'*.geo'));
   handles.geolist = {geo_files.name}; handles.geolist = handles.geolist';
@@ -66,27 +93,21 @@ function handles = setWorkDir(handles, new_dir)
     %set(handles.popupmenu_inputfile,'String',inp_files);
   %end
 
-  clear prn_files geo_files inp_files;
+  clear data_files geo_files inp_files;
   
   %handles.snaplist
 end
 
-function handles = load_data(handles)
+function [ handles, isLoaded ] = load_data(handles)
   disp('function pushbutton_load_data_Callback(hObject, eventdata, handles)')
   % --- Executes on button press in pushbutton_load_data.
   % hObject    handle to pushbutton_load_data (see GCBO)
   % eventdata  reserved - to be defined in a future version of MATLAB
   % handles    structure with handles and user data (see GUIDATA)
-  
-  val = handles.inputsnapshot;
-  if (val<1) | (length(handles.snaplist)<val)
-    return
-  end
-  snapfile = handles.snaplist{val};
-  handles.snapfile = [handles.workdir, filesep, snapfile];
-  
+
   val = handles.geometryfile;
   if (val<1) | (length(handles.geolist)<val)
+    isLoaded = 0;
     return
   end
   geofile = handles.geolist{val};
@@ -94,17 +115,48 @@ function handles = load_data(handles)
   
   val = handles.inputfile;
   if (val<1) | (length(handles.inplist)<val)
+    isLoaded = 0;
     return
   end
   inpfile = handles.inplist{val};
   handles.inpfile = [handles.workdir, filesep, inpfile];
-  
-  %load snapshot data
-  [header, handles.fin1] = hdrload(handles.snapfile);
+
+  if handles.Type == 1
+    val = handles.ProbeID;
+    if (val<1) | (length(handles.ProbeList)<val)
+      isLoaded = 0;
+      return
+    end
+    name = handles.ProbeList{val};
+    handles.ProbeFile = [handles.workdir, filesep, name];
+    %load snapshot data
+    [handles.header, handles.fin1] = hdrload(handles.ProbeFile);
+  elseif handles.Type == 2
+    val = handles.TimeSnapshotID;
+    if (val<1) | (length(handles.TimeSnapshotList)<val)
+      isLoaded = 0;
+      return
+    end
+    name = handles.TimeSnapshotList{val};
+    handles.TimeSnapshotFile = [handles.workdir, filesep, name];
+    [handles.header, handles.fin1] = hdrload(handles.TimeSnapshotFile);
+  elseif handles.Type == 3
+    val = handles.FrequencySnapshotID;
+    if (val<1) | (length(handles.FrequencySnapshotList)<val)
+      isLoaded = 0;
+      return
+    end
+    name = handles.FrequencySnapshotList{val};
+    handles.FrequencySnapshotFile = [handles.workdir, filesep, name];
+    [handles.header, handles.fin1] = hdrload(handles.FrequencySnapshotFile);
+  else
+    error('Unknown data type')
+    return
+  end
   
   %determine orientation of snapshot
   handles.gr = size(handles.fin1);
-  columns = strread(header,'%s');
+  columns = strread(handles.header,'%s');
   if strcmp(columns(1),'y') && strcmp(columns(2),'z')
       handles.plane = 1;
       handles.maxy = handles.fin1(handles.gr(1),1);
@@ -119,8 +171,28 @@ function handles = load_data(handles)
       handles.maxy = handles.fin1(handles.gr(1),2);
   end
   
-  handles.colplot = columns;
+  handles.colplot = columns; % all headers
   columns = columns(3:length(columns));
   columns = char(columns);
-  handles.plotcolumn = columns;
+  handles.plotcolumn = columns; % all headers except the two first ones
+  
+  isLoaded = 1;
+end
+
+function handles = generate_plot(handles)
+  disp('function pushbutton_generate_plot_Callback(hObject, eventdata, handles)')
+  % hObject    handle to pushbutton_generate_plot (see GCBO)
+  % eventdata  reserved - to be defined in a future version of MATLAB
+  % handles    structure with handles and user data (see GUIDATA)
+  
+  %handles.colplot
+  
+  %handles.plotcolumn
+  
+  col = handles.col;
+  %col = col+2;
+  handles.dataname = handles.colplot(col);
+  max = handles.maxplotvalue;
+  
+  plotgen(max,col,handles);
 end
