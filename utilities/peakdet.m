1fa9573a9d120967ec03368caa3acb0b1a1ae3f4
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-04-15 11:32:00 +0100

    synching files with erman's code again... :/ (Erman, please use this repo!)

--- utilities/peakdet.m
+++ utilities/peakdet.m
@@ -1,74 +1,75 @@
 function [maxtab, mintab]=peakdet(v, delta, x)
-%PEAKDET Detect peaks in a vector
-%        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
-%        maxima and minima ("peaks") in the vector V.
-%        MAXTAB and MINTAB consists of two columns. Column 1
-%        contains indices in V, and column 2 the found values.
-%
-%		 i.e.
-%		 maxtab =
-%		 mxpos1 mx1
-%		 mxpos2 mx2
-%		 mxpos3 mx3
-%		 mintab =
-%		 mnpos1 mn1
-%		 mnpos2 mn2
-%		 mnpos3 mn3
-%      
-%        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
-%        in MAXTAB and MINTAB are replaced with the corresponding
-%        X-values.
-%
-%        A point is considered a maximum peak if it has the maximal
-%        value, and was preceded (to the left) by a value lower by
-%        DELTA.
-
-% Eli Billauer, 3.4.05 (Explicitly not copyrighted).
-% This function is released to the public domain; Any use is allowed.
-
-maxtab = [];
-mintab = [];
-
-v = v(:); % Just in case this wasn't a proper vector
-
-if nargin < 3
-  x = (1:length(v))';
-else 
-  x = x(:);
-  if length(v)~= length(x)
-    error('Input vectors v and x must have same length');
+  %PEAKDET Detect peaks in a vector
+  %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+  %        maxima and minima ("peaks") in the vector V.
+  %        MAXTAB and MINTAB consists of two columns. Column 1
+  %        contains indices in V, and column 2 the found values.
+  %
+  %		 i.e.
+  %		 maxtab =
+  %		 mxpos1 mx1
+  %		 mxpos2 mx2
+  %		 mxpos3 mx3
+  %		 mintab =
+  %		 mnpos1 mn1
+  %		 mnpos2 mn2
+  %		 mnpos3 mn3
+  %      
+  %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+  %        in MAXTAB and MINTAB are replaced with the corresponding
+  %        X-values.
+  %
+  %        A point is considered a maximum peak if it has the maximal
+  %        value, and was preceded (to the left) by a value lower by
+  %        DELTA.
+  
+  % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+  % This function is released to the public domain; Any use is allowed.
+  
+  maxtab = [];
+  mintab = [];
+  
+  v = v(:); % Just in case this wasn't a proper vector
+  
+  if nargin < 3
+    x = (1:length(v))';
+  else 
+    x = x(:);
+    if length(v)~= length(x)
+      error('Input vectors v and x must have same length');
+    end
   end
-end
+    
+  if (length(delta(:)))>1
+    error('Input argument DELTA must be a scalar');
+  end
   
-if (length(delta(:)))>1
-  error('Input argument DELTA must be a scalar');
-end
-
-if delta <= 0
-  error('Input argument DELTA must be positive');
-end
-
-mn = Inf; mx = -Inf;
-mnpos = NaN; mxpos = NaN;
-
-lookformax = 1;
-
-for i=1:length(v)
-  this = v(i);
-  if this > mx, mx = this; mxpos = x(i); end
-  if this < mn, mn = this; mnpos = x(i); end
+  if delta <= 0
+    error('Input argument DELTA must be positive');
+  end
   
-  if lookformax
-    if this < mx-delta
-      maxtab = [maxtab ; mxpos mx];
-      mn = this; mnpos = x(i);
-      lookformax = 0;
-    end  
-  else
-    if this > mn+delta
-      mintab = [mintab ; mnpos mn];
-      mx = this; mxpos = x(i);
-      lookformax = 1;
+  mn = Inf; mx = -Inf;
+  mnpos = NaN; mxpos = NaN;
+  
+  lookformax = 1;
+  
+  for i=1:length(v)
+    this = v(i);
+    if this > mx, mx = this; mxpos = x(i); end
+    if this < mn, mn = this; mnpos = x(i); end
+    
+    if lookformax
+      if this < mx-delta
+        maxtab = [maxtab ; mxpos mx];
+        mn = this; mnpos = x(i);
+        lookformax = 0;
+      end  
+    else
+      if this > mn+delta
+        mintab = [mintab ; mnpos mn];
+        mx = this; mxpos = x(i);
+        lookformax = 1;
+      end
     end
   end
 end
