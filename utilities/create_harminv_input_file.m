63ac54a798a66f280780e0e82ed57317047b42db
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-04-15 05:15:03 +0100

    just added datacol arg + unit info

--- utilities/create_harminv_input_file.m
+++ utilities/create_harminv_input_file.m
@@ -1,4 +1,4 @@
-function create_harminv_input_file(INFILE,OUTFILE,PARAMFILE,lambdamin_nm,lambdamax_nm)
+function create_harminv_input_file(INFILE,datacol,OUTFILE,PARAMFILE,lambdamin_nm,lambdamax_nm)
   % create harminv input files from INFILE and store it in OUTFILE and PARAMFILE
 
   if ~(exist(INFILE,'file'))
@@ -8,18 +8,17 @@ function create_harminv_input_file(INFILE,OUTFILE,PARAMFILE,lambdamin_nm,lambdam
   format long e
   [header,data] = hdrload(INFILE);
   % Time Ex Ey Ez Hx Hy Hz 
-  out = data(:,2);
+  out = data(:,datacol);
   save(OUTFILE,'out','-ascii');
   
-  fmin = 10^3*get_c0()/lambdamax_nm;
-  fmax = 10^3*get_c0()/lambdamin_nm;
+  fmin_MHz = 10^3*get_c0()/lambdamax_nm;
+  fmax_MHz = 10^3*get_c0()/lambdamin_nm;
 
-  dt = 1e-12*(data(2,1)-data(1,1));  % data(*,1) being in 10^-18 s, dt is in 10^-18 s/1e-12 = 10^-6 s
+  dt_mus = 1e-12*(data(2,1)-data(1,1));  % data(*,1) being in 10^-18 s, dt is in 10^-18 s/1e-12 = 10^-6 s
           
   %write parameters to file
   file = fopen(PARAMFILE,'w');
-  fprintf('final: dt=%E fmin=%E fmax=%E\n', dt, fmin, fmax);
-  fprintf(file,'final: dt=%E fmin=%E fmax=%E\n', dt, fmin, fmax);
+  fprintf('final: dt=%E fmin=%E fmax=%E\n', dt_mus, fmin_MHz, fmax_MHz);
+  fprintf(file,'final: dt=%E fmin=%E fmax=%E\n', dt_mus, fmin_MHz, fmax_MHz);
   fclose(file);	
 end
-
