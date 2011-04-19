f41faa1ef19d2123cedae3e01d3ffd327a8e92de
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-04-19 10:28:13 +0100

    -added confirmation question to qdel_all/queued -superqsub now only submits scripts where no .out file is present

--- bin/qdel_all.sh
+++ bin/qdel_all.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 set -eux
-qstat | grep $USER | awk -F. '{print $1}' | xargs -n1 qdel
+
+function qdel_all()
+{
+  qstat | grep $USER | awk -F. '{print $1}' | xargs -n1 qdel
+}
+
+echo "delete all your jobs? (y/n)"
+read ans
+case $ans in
+  y|Y|yes) qdel_all;;
+  *) echo "Not doing anything";;
+esac
--- bin/superqsub.sh
+++ bin/superqsub.sh
@@ -10,6 +10,8 @@ set -eux
 # Check if all parameters are present
 # If no, exit
 
+source script_inception_common_functions.sh
+
 echo $#
 
 if [ $# -lt 1 ]
@@ -24,6 +26,23 @@ fi
 
 for SCRIPT in "$@"
 do
-  echo "submitting $SCRIPT"
-  qsub -l nodes=1:ppn=$PPN -M $QSUBMAIL -v JOBDIR="$(readlink -f $(dirname "$SCRIPT"))" $SCRIPT
+  OUTFILE=$(getOutFile "$SCRIPT")
+  
+  set +e
+  getDataState $SCRIPT
+  status=$?
+  set -e
+  
+  if  [ $status == 2 ]
+  then
+    echo "submitting $SCRIPT"
+    qsub -l nodes=1:ppn=$PPN -M $QSUBMAIL -v JOBDIR="$(readlink -f $(dirname "$SCRIPT"))" $SCRIPT
+  else
+    if  [ $status == 0 ]
+    then
+      echo "script already done. -> skipping (remove .out file to run it anyway)"
+    else
+      echo "script seems to be already running. -> skipping (remove .out file to run it anyway)"
+    fi
+  fi
 done
