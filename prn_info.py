7b2df7cc727c43b7c91e14f6840b5ef7ebabc963
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-05-12 15:34:19 +0100

    prn_info.py improvements

--- prn_info.py
+++ prn_info.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import bfdtd.bfdtd_parser
+import sys
 
-toto= bfdtd.bfdtd_parser.readBristolFDTD('rectangular_holes.bottomN_30.topN_15.excitationType_Zm1.in')
+FDTDobj = bfdtd.bfdtd_parser.readBristolFDTD(sys.argv[1])
 
-for i in range(len(toto.frequency_snapshot_list)):
-  print i, toto.frequency_snapshot_list[i].name, toto.frequency_snapshot_list[i].plane
+for i in range(len(FDTDobj.frequency_snapshot_list)):
+  plane = FDTDobj.frequency_snapshot_list[i].plane
+  print 'i+1=', i+1, 'name=', FDTDobj.frequency_snapshot_list[i].name, 'plane=', plane, 'f=', FDTDobj.frequency_snapshot_list[i].frequency, 'pos=', FDTDobj.frequency_snapshot_list[i].P1[plane-1]
