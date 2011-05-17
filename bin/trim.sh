f67b6910238d7f3a13087c24ee3f03275b3a1b85
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-05-17 12:56:10 +0100

    adding image processing script

---
+++ bin/trim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+trimit()
+{
+  OLD=$1
+  NEW=$(dirname $OLD)/$(basename $OLD .eps).png
+  gs -r300 -dEPSCrop -dTextAlphaBits=4 -sDEVICE=png16m -sOutputFile=$NEW -dBATCH -dNOPAUSE $OLD
+  convert $NEW -trim $NEW
+}
+
+#trimit $1
+#find . -name "*.eps" | xargs -n1 -I{} trimit() {}
+
+find . -name "*.eps"  | while read FILENAME;
+do
+  trimit $FILENAME
+done
+
+# convert image1.png \( image2.png  image3.png -append \) -gravity center +append out.png
