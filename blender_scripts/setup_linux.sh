e74544f7941c0bd529f119610440f2a7e0cdc319
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-05-19 09:17:27 +0100

    made caliper executable + disabled blender beta setup

--- blender_scripts/setup_linux.sh
+++ blender_scripts/setup_linux.sh
@@ -5,5 +5,5 @@ for f in *.py;
 do
   echo "Processing $f file...";
   ln -s $(readlink -f "$f") $HOME/.blender/scripts/
-  ln -s $(readlink -f "$f") $HOME/bin/blender-2.56a-beta-linux-glibc27-x86_64/2.56/scripts
+#  ln -s $(readlink -f "$f") $HOME/bin/blender-2.56a-beta-linux-glibc27-x86_64/2.56/scripts
 done
