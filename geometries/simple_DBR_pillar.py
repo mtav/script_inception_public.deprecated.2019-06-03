fed66089d7d1d0da4d4c97fba62f99cb6ecb8a7b
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-06-02 03:10:23 +0100

    working on pillar reboot

--- geometries/simple_DBR_pillar.py
+++ geometries/simple_DBR_pillar.py
@@ -10,50 +10,45 @@ import os
 
 pillar = BFDTDobject()
 
-#########
 # constants
-#########
 n_air = 1
 n_diamond = 2.4
 Lambda_mum = 0.637
 delta = Lambda_mum/(10*n_diamond)
 freq = get_c0()/Lambda_mum
-k=1
-radius = k*Lambda_mum/(4*n_diamond)
-Nbottom = 40
-Ntop = 20
+k=1; radius = k*Lambda_mum/(4*n_diamond)
+Nbottom = 30
+Ntop = 30
 h_air = Lambda_mum/(4*n_air)
 h_diamond = Lambda_mum/(4*n_diamond)
 h_cavity = Lambda_mum/(n_diamond)
 height = Nbottom*(h_air+h_diamond) + h_cavity + Ntop*(h_air+h_diamond)
+buffer = 0.25
+FullBox_upper = [ height+2*buffer, 2*(radius+buffer), 2*(radius+buffer) ]
+P_centre = [ buffer + Nbottom*(h_air+h_diamond) + 0.5*h_cavity, 0.5*FullBox_upper[1], 0.5*FullBox_upper[2] ]
 
-#########
 # define flag
-#########
-pillar.flag.iterations = 25000
+pillar.flag.iterations = 100000
 
-#########
 # define boundary conditions
-#########
 pillar.boundaries.Xpos_bc = 2
-pillar.boundaries.Ypos_bc = 2
+pillar.boundaries.Ypos_bc = 1 #2
 pillar.boundaries.Zpos_bc = 2
 
-#########
 # define box
-#########
 pillar.box.lower = [0,0,0]
-pillar.box.upper = [height+2,2*(radius+1),2*(radius+1)]
+if pillar.boundaries.Ypos_bc == 2:
+  pillar.box.upper = FullBox_upper
+else:
+  pillar.box.upper = [ FullBox_upper[0], 0.5*FullBox_upper[1], FullBox_upper[2] ]
 
-#########
 # define geometry
-#########
-Ymin=pillar.box.getCenter()[1]-radius
-Ymax=pillar.box.getCenter()[1]+radius
-Zmin=pillar.box.getCenter()[2]-radius
-Zmax=pillar.box.getCenter()[2]+radius
+Ymin=P_centre[1]-radius
+Ymax=P_centre[1]+radius
+Zmin=P_centre[2]-radius
+Zmax=P_centre[2]+radius
 
-X_current = 1
+X_current = buffer
 
 for i in range(Nbottom):
   block = Block(lower = [ X_current, Ymin, Zmin ], upper = [ X_current+h_diamond, Ymax, Zmax ], permittivity = pow(n_diamond,2), conductivity = 0)
@@ -75,48 +70,46 @@ for i in range(Ntop):
   pillar.geometry_object_list.append(block)
   X_current = X_current+h_diamond
 
-#########
 # define excitation
-#########
 excitation = Excitation()
-excitation.P1 = [ block.getCenter()[0], block.getCenter()[1]-1*delta, block.getCenter()[2] ]
-excitation.P2 = block.getCenter()
+excitation.P1 = [ P_centre[0], P_centre[1]-1*delta, P_centre[2] ]
+excitation.P2 = P_centre
 excitation.frequency = freq
 excitation.E = list(Unit(subtract(excitation.P2,excitation.P1)))
 pillar.excitation_list = [ excitation ]
 
-#########
 # define probe
-#########
 probe = Probe(position = [ block.upper[0]+delta, block.getCenter()[1], block.getCenter()[2] ])
 pillar.probe_list = [ probe ]
 
-#########
 # define frequency snapshots
-#########
 first = min(65400,pillar.flag.iterations)
 frequency_vector = [freq]
 F = pillar.addFrequencySnapshot(1,block.getCenter()[0]); F.first = first; F.frequency_vector = frequency_vector
-F = pillar.addFrequencySnapshot(2,block.getCenter()[1]); F.first = first; F.frequency_vector = frequency_vector
+if pillar.boundaries.Ypos_bc == 2:
+  F = pillar.addFrequencySnapshot(2,block.getCenter()[1]); F.first = first; F.frequency_vector = frequency_vector
+else:
+  F = pillar.addFrequencySnapshot(2,block.getCenter()[1]-delta); F.first = first; F.frequency_vector = frequency_vector
 F = pillar.addFrequencySnapshot(3,block.getCenter()[2]); F.first = first; F.frequency_vector = frequency_vector
 F = pillar.addBoxFrequencySnapshots(); F.first = first; F.frequency_vector = frequency_vector
 
-#########
 # define mesh
-#########
-#max_delta_Vector_X = [ delta, delta, delta, delta ]
-max_delta_Vector_Y = [ delta, delta, delta, delta, delta, delta ]
+max_delta_Vector_X = [ delta, delta, delta, delta, delta ]
+thicknessVector_X = [ block.lower[0]-pillar.box.lower[0], P_centre[0]-block.lower[0], block.upper[0]-P_centre[0], delta, pillar.box.upper[0]-(block.upper[0]+delta) ]
+if pillar.boundaries.Ypos_bc == 2:
+  max_delta_Vector_Y = [ delta, delta, delta, delta, delta, delta ]
+  thicknessVector_Y = [ block.lower[1]-pillar.box.lower[1], (block.getCenter()[1]-delta)-block.lower[1], delta, delta, block.upper[1]-(block.getCenter()[1]+delta), pillar.box.upper[1]-block.upper[1] ]
+else:
+  max_delta_Vector_Y = [ delta, delta, delta ]
+  thicknessVector_Y = [ block.lower[1]-pillar.box.lower[1], (block.getCenter()[1]-delta)-block.lower[1], delta ]
 max_delta_Vector_Z = [ delta, delta, delta, delta ]
-#thicknessVector_X = LimitsToThickness([ pillar.box.lower[0], block.lower[0], block.upper[0], block.upper[0]+delta, pillar.box.upper[0] ])
-#thicknessVector_Y = [ block.lower[1]-pillar.box.lower[1], (block.getCenter()[1]-delta)-block.lower[1], delta, delta, block.upper[1]-(block.getCenter()[1]+delta), pillar.box.upper[1]-block.upper[1] ]
-#thicknessVector_Z = LimitsToThickness([ pillar.box.lower[2], block.lower[2], block.getCenter()[2], block.upper[2], pillar.box.upper[2] ])
-#pillar.delta_X_vector, local_delta_X_vector = subGridMultiLayer(max_delta_Vector_X,thicknessVector_X)
-#pillar.delta_Y_vector, local_delta_Y_vector = subGridMultiLayer(max_delta_Vector_Y,thicknessVector_Y)
-#pillar.delta_Z_vector, local_delta_Z_vector = subGridMultiLayer(max_delta_Vector_Z,thicknessVector_Z)
+thicknessVector_Z = LimitsToThickness([ pillar.box.lower[2], block.lower[2], block.getCenter()[2], block.upper[2], pillar.box.upper[2] ])
+  
+pillar.delta_X_vector, local_delta_X_vector = subGridMultiLayer(max_delta_Vector_X,thicknessVector_X)
+pillar.delta_Y_vector, local_delta_Y_vector = subGridMultiLayer(max_delta_Vector_Y,thicknessVector_Y)
+pillar.delta_Z_vector, local_delta_Z_vector = subGridMultiLayer(max_delta_Vector_Z,thicknessVector_Z)
 
-#########
 # write
-#########
 #DSTDIR = os.getenv('DATADIR')
 DSTDIR = os.getenv('TESTDIR')
 BASENAME = 'simple_DBR_pillar'
--- geometries/simple_pillar.py
+++ geometries/simple_pillar.py
@@ -11,23 +11,28 @@ import os
 pillar = BFDTDobject()
 
 # constants
-n_Diamond = 2.4
+n_air = 1
+n_diamond = 2.4
 Lambda_mum = 0.637
-delta = Lambda_mum/(10*n_Diamond)
+delta = Lambda_mum/(10*n_diamond)
 freq = get_c0()/Lambda_mum
-radius = 0.25
-height = 2.*20.*Lambda_mum/(2.*n_Diamond) + Lambda_mum/n_Diamond
+k=1; radius = k*Lambda_mum/(4*n_diamond)
+Nbottom = 30
+Ntop = 30
+h_air = Lambda_mum/(4*n_air)
+h_diamond = Lambda_mum/(4*n_diamond)
+h_cavity = Lambda_mum/(n_diamond)
+height = Nbottom*(h_air+h_diamond) + h_cavity + Ntop*(h_air+h_diamond)
 buffer = 0.25
 FullBox_upper = [ height+2*buffer, 2*(radius+buffer), 2*(radius+buffer) ]
-P_centre = [ 0.5*FullBox_upper[0], 0.5*FullBox_upper[1], 0.5*FullBox_upper[2] ]
+P_centre = [ buffer + Nbottom*(h_air+h_diamond) + 0.5*h_cavity, 0.5*FullBox_upper[1], 0.5*FullBox_upper[2] ]
 
 # define flag
 pillar.flag.iterations = 100000
 
 # define boundary conditions
 pillar.boundaries.Xpos_bc = 2
-#pillar.boundaries.Ypos_bc = 2
-pillar.boundaries.Ypos_bc = 1
+pillar.boundaries.Ypos_bc = 1 #2
 pillar.boundaries.Zpos_bc = 2
 
 # define box
@@ -37,18 +42,18 @@ if pillar.boundaries.Ypos_bc == 2:
 else:
   pillar.box.upper = [ FullBox_upper[0], 0.5*FullBox_upper[1], FullBox_upper[2] ]
 
-# define block
+# define geometry
 block = Block()
 block.lower = [ P_centre[0]-0.5*height, P_centre[1]-radius, P_centre[2]-radius ]
 block.upper = [ P_centre[0]+0.5*height, P_centre[1]+radius, P_centre[2]+radius ]
-block.permittivity = pow(n_Diamond,2)
+block.permittivity = pow(n_diamond,2)
 block.conductivity = 0
 pillar.geometry_object_list = [ block ]
 
 # define excitation
 excitation = Excitation()
-excitation.P1 = [ block.getCenter()[0], block.getCenter()[1]-1*delta, block.getCenter()[2] ]
-excitation.P2 = block.getCenter()
+excitation.P1 = [ P_centre[0], P_centre[1]-1*delta, P_centre[2] ]
+excitation.P2 = P_centre
 excitation.frequency = freq
 excitation.E = list(Unit(subtract(excitation.P2,excitation.P1)))
 pillar.excitation_list = [ excitation ]
@@ -62,15 +67,15 @@ first = min(65400,pillar.flag.iterations)
 frequency_vector = [freq]
 F = pillar.addFrequencySnapshot(1,block.getCenter()[0]); F.first = first; F.frequency_vector = frequency_vector
 if pillar.boundaries.Ypos_bc == 2:
-  F = pillar.addFrequencySnapshot(2,block.getCenter()[1]-delta); F.first = first; F.frequency_vector = frequency_vector
+  F = pillar.addFrequencySnapshot(2,block.getCenter()[1]); F.first = first; F.frequency_vector = frequency_vector
 else:
   F = pillar.addFrequencySnapshot(2,block.getCenter()[1]-delta); F.first = first; F.frequency_vector = frequency_vector
 F = pillar.addFrequencySnapshot(3,block.getCenter()[2]); F.first = first; F.frequency_vector = frequency_vector
 F = pillar.addBoxFrequencySnapshots(); F.first = first; F.frequency_vector = frequency_vector
 
 # define mesh
-max_delta_Vector_X = [ delta, delta, delta, delta ]
-thicknessVector_X = LimitsToThickness([ pillar.box.lower[0], block.lower[0], block.upper[0], block.upper[0]+delta, pillar.box.upper[0] ])
+max_delta_Vector_X = [ delta, delta, delta, delta, delta ]
+thicknessVector_X = [ block.lower[0]-pillar.box.lower[0], P_centre[0]-block.lower[0], block.upper[0]-P_centre[0], delta, pillar.box.upper[0]-(block.upper[0]+delta) ]
 if pillar.boundaries.Ypos_bc == 2:
   max_delta_Vector_Y = [ delta, delta, delta, delta, delta, delta ]
   thicknessVector_Y = [ block.lower[1]-pillar.box.lower[1], (block.getCenter()[1]-delta)-block.lower[1], delta, delta, block.upper[1]-(block.getCenter()[1]+delta), pillar.box.upper[1]-block.upper[1] ]
@@ -87,7 +92,7 @@ pillar.delta_Z_vector, local_delta_Z_vector = subGridMultiLayer(max_delta_Vector
 # write
 #DSTDIR = os.getenv('DATADIR')
 DSTDIR = os.getenv('TESTDIR')
-BASENAME = 'simple_pillar_2'
+BASENAME = 'simple_pillar'
 pillar.writeAll(DSTDIR+os.sep+BASENAME, BASENAME)
 GEOshellscript_advanced(DSTDIR+os.sep+BASENAME+os.sep+BASENAME+'.sh', BASENAME, getProbeColumnFromExcitation(excitation.E),'$HOME/bin/fdtd', '$JOBDIR', WALLTIME = 360)
 print pillar.getNcells()
