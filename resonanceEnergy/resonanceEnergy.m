7bb089f60ec27d4ede10e278254056d5b8880a09
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-06-20 17:16:13 +0100

    added new mode plots, including neff(radius)

--- resonanceEnergy/resonanceEnergy.m
+++ resonanceEnergy/resonanceEnergy.m
@@ -1,4 +1,4 @@
-function [ E_meV, lambda_nm, radius_vector_mum, E_vector_meV, lambda_vector_nm ] = resonanceEnergy(n_cavity, n_mirror, n_outside, Lcav_nm, radius)
+function [ E_meV, lambda_nm, real_neff, radius_vector_mum, E_vector_meV, lambda_vector_nm, real_neff_vector, u, v, b, v_cutoff ] = resonanceEnergy(n_cavity, n_mirror, n_outside, Lcav_nm, radius, L, M)
   %resonanceEnergy(3.521,2.973,1,253,radius)
   %input:
   % n_cavity=3.521;%no unit
@@ -11,8 +11,8 @@ function [ E_meV, lambda_nm, radius_vector_mum, E_vector_meV, lambda_vector_nm ]
   % lambda_nm (nm)
   
   % Mode LPlm
-  L = 0;
-  M = 1;
+  %L = 0;
+  %M = 1;
 
   racines_l = zero_besselj(L);
   racines_l_moins_1 = zero_besselj(L-1);
@@ -60,26 +60,28 @@ function [ E_meV, lambda_nm, radius_vector_mum, E_vector_meV, lambda_vector_nm ]
     end 
   end
 
-  b = 1 - (u.^2 ./ v'.^2); % = u^2/v^2
+  b = 1 - (u.^2 ./ v'.^2); % normalized propagation constant
   w = sqrt(v'.^2 - u.^2);
 
   %%%%%%%%%%%%%%%%%%%%%%
   % calculate lambda_nm and E_meV as a function of the obtained u values (and the related v,w,b)
   %%%%%%%%%%%%%%%%%%%%%%
-  neff = get_neff(n_cavity, n_mirror); % average refractive index
-  lambda_vector_nm = sqrt(b.*(neff^2-n_outside^2)+n_outside^2)*Lcav_nm*(n_cavity/neff); % (nm)
+  approx_neff = get_neff(n_cavity, n_mirror); % average refractive index
+  lambda_vector_nm = sqrt(b.*(approx_neff^2-n_outside^2)+n_outside^2)*Lcav_nm*(n_cavity/approx_neff); % (nm)
   E_vector_eV = (get_h()*get_c0()/get_e())./(lambda_vector_nm.*10^(-9)); %energy (eV)
   E_vector_meV = E_vector_eV'*1000; % energy (meV)
   k0 = 2*pi./lambda_vector_nm; % free space wave number
-  kn = k0.*sqrt(neff^2-n_outside^2); 
+  kn = k0.*sqrt(approx_neff^2-n_outside^2); 
   radius_vector_mum = v./(kn'*1000); % radius (mum) of micropillar microcavity (kn in nm^-1 and v has no unit)
+  real_neff_vector = n_mirror + b*(n_cavity-n_mirror);
   
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % Calculate E_meV and lambda_nm for the values of radius (input argument) using simple interpolation
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-  E_meV=[];
-  lambda_nm=[];
+  
+  E_meV = [];
+  lambda_nm = [];
+  real_neff = [];
   for i=1:length(radius)
     r = radius(i);
     idx = find(min(abs(radius_vector_mum-r))==abs(radius_vector_mum-r));
@@ -87,17 +89,21 @@ function [ E_meV, lambda_nm, radius_vector_mum, E_vector_meV, lambda_vector_nm ]
       if idx-1>0
         E_meV = [E_meV, interpolate(radius_vector_mum, E_vector_meV, idx-1, idx, r)];
         lambda_nm = [lambda_nm, interpolate(radius_vector_mum, lambda_vector_nm, idx-1, idx, r)];
+        real_neff = [real_neff, interpolate(radius_vector_mum, real_neff_vector, idx-1, idx, r)];
       else
         E_meV = [E_meV, E_vector_meV(1)];
         lambda_nm = [lambda_nm, lambda_vector_nm(1)];
+        real_neff = [real_neff, real_neff_vector(1)];
       end
     else
       if idx+1 <= length(radius_vector_mum)
         E_meV = [E_meV, interpolate(radius_vector_mum, E_vector_meV, idx, idx+1, r)];
         lambda_nm = [lambda_nm, interpolate(radius_vector_mum, lambda_vector_nm, idx, idx+1, r)];
+        real_neff = [real_neff, interpolate(radius_vector_mum, real_neff_vector, idx, idx+1, r)];
       else
         E_meV = [E_meV, E_vector_meV(length(radius_vector_mum))];
         lambda_nm = [lambda_nm, lambda_vector_nm(length(radius_vector_mum))];
+        real_neff = [real_neff, real_neff_vector(length(radius_vector_mum))];
       end
     end
   end
