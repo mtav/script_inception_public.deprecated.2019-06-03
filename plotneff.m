e52aaa387b613e0afafc23b65c32b68876da4055
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-06-24 17:30:30 +0100

    more plots... ^^

--- plotneff.m
+++ plotneff.m
@@ -5,6 +5,8 @@ function plotneff()
   n_outside = 1;
   lambda_nm = 637;
 
+  m_reflectivity = 40;
+
   radius_mum = [0:0.1:5];
   figure;
   
@@ -53,39 +55,105 @@ function plotneff()
 
   v_cutoff_mat
 
+  %%%%%%%%%%%
+  %radius_mum = [0:0.1:5];
+  radius_mum = [0.25:0.01:1.5];
+
+  delta = [0:0.01:1.5];
   rad1 = radius_mum;
   rad2 = radius_mum;
+
   [Rad1, Rad2]=meshgrid(rad1,rad2);
-  [r, rg] = reflectivity(get_neff_2(Rad1),get_neff_2(Rad2),1);
+  [r_radrad, rg_radrad] = reflectivity(get_neff_2(Rad1),get_neff_2(Rad2),m_reflectivity);
   
   figure;
-  surf(Rad1,Rad2,r);
+  subplot(2,1,1);
+  surf(Rad1,Rad2,r_radrad);
   title('r');
   xlabel('radius 1 (mum)');
   ylabel('radius 2 (mum)');
   zlabel('r (no unit)');
   shading interp;
+  colorbar;
   
-  figure;
-  surf(Rad1,Rad2,rg);
+  subplot(2,1,2);
+  surf(Rad1,Rad2,rg_radrad);
   title('rg');
   xlabel('radius 1 (mum)');
   ylabel('radius 2 (mum)');
   zlabel('rg (no unit)');
   shading interp;
+  colorbar;
 
+  %%%%%%%%%%%
+  [Rad1, Delta]=meshgrid(rad1,delta);
+  [r_raddelta, rg_raddelta] = reflectivity(get_neff_2(Rad1),get_neff_2(Rad1+Delta),m_reflectivity);
+
+  figure;
+  subplot(2,1,1);
+  surf(Rad1,Delta,r_raddelta);
+  title('r');
+  xlabel('radius 1 (mum)');
+  ylabel('rad2-rad1 (mum)');
+  zlabel('r (no unit)');
+  shading interp;
+  colorbar;
+
+  subplot(2,1,2);
+  surf(Rad1,Delta,rg_raddelta);
+  title('rg');
+  xlabel('radius 1 (mum)');
+  ylabel('rad2-rad1 (mum)');
+  zlabel('rg (no unit)');
+  shading interp;
+  colorbar;
+
+  %%%%%%%%%%%
+  rad1 = 0.5;%[0.25:0.01:1.5];
   delta = [0:0.01:1];
-  [r, rg] = reflectivity(get_neff_2(0.5),get_neff_2(0.5+delta),1)
+  [r, rg] = reflectivity(get_neff_2(rad1),get_neff_2(rad1+delta),m_reflectivity);
   
   figure;
   grid on; hold on;
-  title('reflectivity as a function of delta=rad2-rad1 for rad1=1mum and m=1');
+  title(['reflectivity as a function of delta=rad2-rad1 for rad1=',num2str(rad1),' mum and m=',num2str(m_reflectivity)]);
   xlabel('rad2-rad1 (mum)');
   ylabel('reflectivity (no unit)');
   plot(delta,r,'r-');
   plot(delta,rg,'b-');
   legend('r','rg');
 
-  [r, rg] = reflectivity(get_neff_2(0.5),get_neff_2(0.6),1)
+  %%%%%%%%%%%
+  rad1 = linspace(0.25,1.5,7);
+  delta = [0:0.01:1];
+  
+  r_all = zeros(length(delta),length(rad1));
+  rg_all = zeros(length(delta),length(rad1));
+  for idx=1:length(rad1)
+    [r, rg] = reflectivity(get_neff_2(rad1(idx)),get_neff_2(rad1(idx)+delta),m_reflectivity);
+    r_all(:,idx) = r;
+    rg_all(:,idx) = rg;
+  end
+  
+  figure;
+  subplot(2,1,1);
+  grid on; hold on;
+  title(['reflectivity r as a function of delta=rad2-rad1 for m=',num2str(m_reflectivity),' and various radiuses']);
+  xlabel('rad2-rad1 (mum)');
+  ylabel('reflectivity r (no unit)');
+  plot(delta,r_all);
+  legend([repmat('rad1 = ',length(rad1'),1), num2str(rad1')]);
+
+  subplot(2,1,2);
+  grid on; hold on;
+  title(['reflectivity rg as a function of delta=rad2-rad1 for m=',num2str(m_reflectivity),' and various radiuses']);
+  xlabel('rad2-rad1 (mum)');
+  ylabel('reflectivity rg (no unit)');
+  plot(delta,rg_all);
+  legend([repmat('rad1 = ',length(rad1'),1), num2str(rad1')]);
+
+  %%%%%%%%%%%
+  neff1 = get_neff_2(0.5)
+  neff2 = get_neff_2(0.6)
+  [r, rg] = reflectivity(get_neff_2(0.5),get_neff_2(0.6),m_reflectivity)
 
 end
