187ea77133e8c584d1df082b1d6d53f4bc78a749
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-10-04 14:54:44 +0100

    name support for GEO_INP_reader.m working

--- bris2meep_matlab/GEO_INP_reader.m
+++ bris2meep_matlab/GEO_INP_reader.m
@@ -77,18 +77,29 @@ function [ entries, structured_entries ] = single_GEO_INP_reader(filename, entri
   fulltext = fileread(filename);
 
   % remove comments
-  pattern_stripcomments = '\*\*.*$';
+  pattern_stripcomments = '\*\*(?!name=).*\n';
   cleantext =  regexprep(fulltext, pattern_stripcomments, '\n', 'lineanchors', 'dotexceptnewline', 'warnings');
 
   % extract blocks
-  pattern_blocks = '^(?<type>\w+).*?\{(?<data>[^\{\}]*?)\}';
-  [tokens_blocks match_blocks names_blocks] =  regexp(cleantext, pattern_blocks, 'tokens', 'match', 'names', 'lineanchors', 'warnings');
+  pattern_objects = '^(?<type>\w+)\s*(?<nameblob>[^\{\}]+)?\{(?<data>[^\{\}]*?)\}';
+  [tokens_blocks match_blocks names_blocks] =  regexp(cleantext, pattern_objects, 'tokens', 'match', 'names', 'lineanchors', 'warnings');
 
   % process blocks
-  disp(['length(names_blocks) = ', num2str(length(names_blocks))]);
+  %disp(['length(names_blocks) = ', num2str(length(names_blocks))]);
   for i = 1:length(names_blocks)
 
     type = names_blocks(:,i).type;
+    nameblob = names_blocks(:,i).nameblob;
+    
+    name = '';
+    if strcmpi(nameblob,'') == 0
+      pattern_nameblob = '\*\*name=(?<name>.*)';
+      [tokens_nameblob match_nameblob names_nameblob] =  regexp(nameblob, pattern_nameblob, 'tokens', 'match', 'names', 'lineanchors', 'warnings');
+      if length(names_nameblob.name) > 0
+        name = strtrim(names_nameblob.name);
+      end
+    end
+    
     data = names_blocks(:,i).data;
     % disp(['===>type = ',type]);
 
@@ -110,23 +121,23 @@ function [ entries, structured_entries ] = single_GEO_INP_reader(filename, entri
 
       % TODO: Check if this can't be simplified, or if it's even necessary.
       if cellFlag
-        if length(num_val)  %% num_val is num
+        if length(num_val) %% num_val is num
           dataV{length(dataV)+1} = num_val;
-        else           %% num_val is not num
+        else %% num_val is not num
           dataV{length(dataV)+1} = str_val;
         end
       else
-         if length(num_val)  %% num_val is num
+         if length(num_val) %% num_val is num
           dataV = [dataV,num_val];
-        else           %% num_val is not num
+        else %% num_val is not num
           cellFlag = 1;
           dataV = num2cell(dataV);
           dataV{length(dataV)+1} = str_val;
         end
       end
     end % end of loop through lines
-
-    entry.name = 'MUMU';
+    
+    entry.name = name;
     entry.type = type;
     entry.data = dataV';
     entries{length(entries)+1} = entry;
@@ -404,21 +415,31 @@ function current_excitation = add_excitation(entry)
   current_excitation.frequency = entry.data(idx); idx = idx+1;
   current_excitation.param1 = entry.data(idx); idx = idx+1;
   current_excitation.param2 = entry.data(idx); idx = idx+1;
-  current_excitation.template_filename = char(entry.data(idx)); idx = idx+1;
+
+  if cell2mat(entry.data(idx))==0
+    current_excitation.template_filename = 'template_filename'; idx = idx+1;
+  else
+    current_excitation.template_filename = char(entry.data(idx)); idx = idx+1;
+  end
+
   current_excitation.template_source_plane = entry.data(idx); idx = idx+1;
+
   if idx<=length(entry.data)
     current_excitation.template_target_plane = entry.data(idx); idx = idx+1;
   else
     current_excitation.template_target_plane = 'x'; idx = idx+1;
   end
+
   if idx<=length(entry.data)
     current_excitation.template_direction = entry.data(idx); idx = idx+1;
   else
     current_excitation.template_direction = 0; idx = idx+1;
   end
+
   if idx<=length(entry.data)
     current_excitation.template_rotation = entry.data(idx); idx = idx+1;
   else
     current_excitation.template_rotation = 0; idx = idx+1;
   end
+
 end
