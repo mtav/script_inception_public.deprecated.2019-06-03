5c5e52ed4b02d4c07e44f2d416be02fe2f8a4989
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-10-05 16:25:38 +0100

    -added main_example.py for reference (warning: not python3 compatible) -added copyBFDTD.py+efficiency_run.py -cleaned resonance_run.py

--- bin/copyBFDTD.py
+++ bin/copyBFDTD.py
@@ -5,10 +5,10 @@
 import re
 from bfdtd.bfdtd_parser import *
 
-def resonance_run(src, dst, freqListFile):
-  ''' Copy src to dst with added frequency snapshots from freqListFile '''
-  src = os.path.abspath(src).rstrip(os.sep)
-  dst = os.path.abspath(dst).rstrip(os.sep)
+def copyBFDTD(src,dst):
+  ''' Copy src to dst '''
+  src = src.rstrip(os.sep)
+  dst = dst.rstrip(os.sep)
   if os.path.isdir(src):
     print src +' is a directory'
     FDTDobj = readBristolFDTD(src+os.sep+os.path.basename(src)+'.in')
@@ -18,18 +18,13 @@ def resonance_run(src, dst, freqListFile):
     FDTDobj = readBristolFDTD(src)
     fileBaseName = os.path.splitext(os.path.basename(src))[0]
   
-  freq_snapshots = getFrequencies(freqListFile)
-  for obj in FDTDobj.frequency_snapshot_list:
-    obj.frequency_vector = freq_snapshots
-  
   FDTDobj.writeAll(dst,fileBaseName)
 
 def main(argv=None):
   # main function
   src = sys.argv[1]
   dst = sys.argv[2]
-  freqListFile = sys.argv[3]
-  resonance_run(src, dst, freqListFile)
+  copyBFDTD(src, dst)
 
 if __name__ == "__main__":
   sys.exit(main())
--- bin/efficiency_run.py
+++ bin/efficiency_run.py
@@ -5,7 +5,7 @@
 import re
 from bfdtd.bfdtd_parser import *
 
-def resonance_run(src, dst, freqListFile):
+def efficiency_run(src, dst):
   ''' Copy src to dst with added frequency snapshots from freqListFile '''
   src = os.path.abspath(src).rstrip(os.sep)
   dst = os.path.abspath(dst).rstrip(os.sep)
@@ -17,19 +17,15 @@ def resonance_run(src, dst, freqListFile):
     print src +' is not a directory'
     FDTDobj = readBristolFDTD(src)
     fileBaseName = os.path.splitext(os.path.basename(src))[0]
-  
-  freq_snapshots = getFrequencies(freqListFile)
-  for obj in FDTDobj.frequency_snapshot_list:
-    obj.frequency_vector = freq_snapshots
-  
+    
+  FDTDobj.geometry_object_list[:] = []
   FDTDobj.writeAll(dst,fileBaseName)
 
 def main(argv=None):
   # main function
   src = sys.argv[1]
   dst = sys.argv[2]
-  freqListFile = sys.argv[3]
-  resonance_run(src, dst, freqListFile)
+  efficiency_run(src, dst)
 
 if __name__ == "__main__":
   sys.exit(main())
