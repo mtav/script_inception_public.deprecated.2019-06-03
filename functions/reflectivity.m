5c2dc29d75c95e1914d0f1227f76ef4e046d3f5d
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2011-10-10 19:22:05 +0100

    page numbers

--- functions/reflectivity.m
+++ functions/reflectivity.m
@@ -1,5 +1,5 @@
 function [ r, rg ] = reflectivity(n1,n2,m)
-  % cf "Diode lasers and photonic integrated cicrcuits by Larry A. Coldren + Scott W. Corzine, p 86+91+104"
+  % cf "Diode lasers and photonic integrated cicrcuits by Larry A. Coldren + Scott W. Corzine, p 86 + 91 + 102-105"
   
   r = (n2-n1)./(n2+n1);
   %rg = tanh(m*ln((1+r)/(1-r)))
