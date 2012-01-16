29af19b85f35f64e3c132b6eeee7a3897c940eb8
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-01-16 10:00:58 +0000

    added support for include, movestageX+movestageY

--- GWL/GWL_parser.py
+++ GWL/GWL_parser.py
@@ -8,6 +8,7 @@ from numpy import *
 class GWLobject:
   def __init__(self):
     self.GWL_voxels = []
+    self.voxel_offset = [0,0,0,0]
   
   def addLine(self,P1,P2):
     write_sequence = [P1,P2]
@@ -25,32 +26,44 @@ class GWLobject:
         #print line
         line_stripped = line.strip()
         # TODO: handle comments and other commands
-        if len(line_stripped)>0:
+        if len(line_stripped)>0 and line_stripped[0]!='%':
           cmd = re.split('[^a-zA-Z0-9_+-.]+',line_stripped)
-          cmd = [ i.lower() for i in cmd ]
+          #cmd = [ i.lower() for i in cmd ]
           #print cmd
           if re.match(r"[a-zA-Z]",cmd[0][0]) or cmd[0]=='-999':
             #print '=>COMMAND'
-            if cmd[0]=='-999':
+            if cmd[0].lower()=='-999':
               if cmd[1]=='-999':
                 #print 'write'
                 self.GWL_voxels.append(write_sequence)
                 write_sequence = []
             else:
-              if cmd[0]=='write':
+              if cmd[0].lower()=='write':
                 #print 'write'
                 self.GWL_voxels.append(write_sequence)
                 write_sequence = []
-              #elif cmd[0]=='defocusfactor':
+              elif cmd[0].lower()=='include':
+                print 'including cmd[1]'
+                self.readGWL(cmd[1])
+              elif cmd[0].lower()=='movestagex':
+                print 'Moving X by '+cmd[1]
+                self.voxel_offset[0] = self.voxel_offset[0] + float(cmd[1])
+              elif cmd[0].lower()=='movestagey':
+                print 'Moving Y by '+cmd[1]
+                self.voxel_offset[1] = self.voxel_offset[1] + float(cmd[1])
+              #elif cmd[0].lower()=='defocusfactor':
                 #print 'defocusfactor'
-              #elif cmd[0]=='laserpower':
+              #elif cmd[0].lower()=='laserpower':
                 #print 'laserpower'
-              #else:
-                #print('UNKNOWN COMMAND: '+cmd[0])
+              else:
+                print('UNKNOWN COMMAND: '+cmd[0])
                 #sys.exit(-1)
           else:
             #print '=>VOXEL'
-            voxel = [ float(i) for i in cmd ]
+            voxel = []
+            for i in range(len(cmd)):
+              voxel.append(float(cmd[i])+self.voxel_offset[i])
+            #voxel = [ float(i) for i in cmd ]
             write_sequence.append(voxel)
             Nvoxels = Nvoxels + 1
             
