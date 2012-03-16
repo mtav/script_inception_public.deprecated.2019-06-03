9dafd95711aa26fdc4e830c072565e1d5170e674
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-03-16 21:04:02 +0000

    fixed error when multiple Q factors are found for one frequency by harminv

--- utilities/getQfactor_harminv.m
+++ utilities/getQfactor_harminv.m
@@ -9,9 +9,27 @@ function Q_harminv_local = getQfactor_harminv(x, harminvDataFile, dt_mus, lambda
       rel=1./err; rel=rel/max(rel)*max(Q);
       
       [indS,val] = closestInd(lambdaH_nm,x)
-      Q_harminv_local = Q(indS);
+      
+      if length(indS)==0
+        error('no closest index found!!!');
+      end
+      
+      for i = 1:length(indS)
+        idx = indS(i)
+        if i == 1
+          Q_harminv_local = Q(idx);
+          err_last = err(idx)
+        else
+          if(err(idx)<err_last)
+            Q_harminv_local = Q(idx);
+            err_last = err(idx)
+          end
+        end
+      end
+      
       peakWaveLength_nm = x;
       Frequency_Hz = get_c0()/peakWaveLength_nm*1e9;
+      
     else
       Q_harminv_local = -1;
       warning('Failed to get Q factor from harminv.');
