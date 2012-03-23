4a99ce84f75105cc20eefc2d0b266688149dcb14
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-03-23 19:29:21 +0000

    some more theoretical excitation analysis

--- propagation_analysis.m
+++ propagation_analysis.m
@@ -1,6 +1,6 @@
 function propagation_analysis(INFILENAME,AMPLITUDE,TIME_OFFSET,TIME_CONSTANT,FREQUENCY)
 
-    figure()
+    %figure()
 
     % x=0.25;
     % delta=20*1/FREQUENCY;t=[TIME_OFFSET-delta/2:delta/100:TIME_OFFSET+delta/2];
@@ -25,7 +25,7 @@ function propagation_analysis(INFILENAME,AMPLITUDE,TIME_OFFSET,TIME_CONSTANT,FRE
 
     delta=20*1/FREQUENCY;
     t_init=[TIME_OFFSET-delta/2:delta/(100*20):TIME_OFFSET+delta/2];
-    hold on;
+    %hold on;
     % x=0;
     % t=t_init+x/get_c0();
     % plot(t,AMPLITUDE*exp(-log(2)*(((t-x/get_c0())-TIME_OFFSET)/TIME_CONSTANT).^2).*sin(2*pi*FREQUENCY*(t-x/get_c0())));
@@ -34,8 +34,53 @@ function propagation_analysis(INFILENAME,AMPLITUDE,TIME_OFFSET,TIME_CONSTANT,FRE
     % toto(10);
 
     function plot_theory(x,style)
-        t=t_init+x/get_c0();
-        plot(t,AMPLITUDE*exp(-log(2)*(((t-x/get_c0())-TIME_OFFSET)/TIME_CONSTANT).^2).*sin(2*pi*FREQUENCY*(t-x/get_c0())),style);
+        fftfig = figure();
+    
+        %t = t_init+x/get_c0();
+        %y = AMPLITUDE*exp(-log(2)*(((t-x/get_c0())-TIME_OFFSET)/TIME_CONSTANT).^2).*sin(2*pi*FREQUENCY*(t-x/get_c0()));
+        
+        delta = 20*1/FREQUENCY;
+        t = [-delta/2:delta/(100*20):delta/2];
+        alpha = log(2)/TIME_CONSTANT^2;        
+        y = exp(-alpha*t.^2).*sin(2*pi*FREQUENCY*t);
+        %y = exp(-alpha*t.^2);
+        
+        subplot(1,3,1)
+        plot(t,y,style);
+        
+        %% FFT for frequency estimation
+        Vfit = y;
+        time_length = t(end)-t(1);
+        sample_int = time_length/(length(t)-1);
+        
+        LV = length(Vfit);
+        P = abs(fft(Vfit)); Ppos=P(1:(round(LV/2)+1));
+        faxis = 1/sample_int*(0:round(LV/2))/LV;
+        peakf = faxis(find(Ppos==max(P(1:(round(LV/2)+1)))));
+        
+        subplot(1,3,2);
+        semilogy(faxis,Ppos,'-'); hold on;
+
+        plot(peakf,max(Ppos),'dr');
+        title('FFT'); xlabel('Frequency [Hz]'); ylabel('Power')
+        fa=axis; text((fa(2)-fa(1))/2,(fa(4)-fa(3))/2,['f_0: ' num2str(peakf,'%.1f') ' MHz',' lambda: ' num2str(get_c0()/peakf,'%.1f') ' mum']);
+
+        subplot(1,3,3);
+        lambda = get_c0()./faxis;
+        plot(lambda,Ppos,'bs'); hold on;
+        
+        %yfit = AMPLITUDE*0.5*sqrt(pi*TIME_CONSTANT^2/log(2))*exp(-(pi*(faxis-FREQUENCY)*TIME_CONSTANT).^2/log(2));
+        %yfit = 0.5*sqrt(pi/alpha)*exp(-(pi*(faxis-FREQUENCY)).^2/alpha);
+        yfit = max(Ppos)*exp(-(pi*(faxis-FREQUENCY)).^2/alpha);
+        plot(lambda,yfit,'r-'); hold on;
+        
+        LV
+        fit_max = 0.5*sqrt(pi/alpha)
+        fft_max1 = max(P(1:(round(LV/2)+1)))
+        fft_max2 = max(Ppos)
+        alpha
+        maxtheo = sqrt(pi/alpha)
+
     end
     
     function plot_simulation(file)
