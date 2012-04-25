735a3738a6eacf872c48cbb8221250b0036bc2ec
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-04-25 17:23:30 +0100

    -added ExcitationUniform class -out_col_name is now a list of strings instead of a 3D vector (the list of columns to fill)

--- bfdtd/excitationTemplate.py
+++ bfdtd/excitationTemplate.py
@@ -5,7 +5,9 @@ from __future__ import division
 from bfdtd.bfdtd_parser import *
 
 import sys
-  
+
+# TODO: Create template parent class, improve class names of excitations and templates, review the whole excitation+template system
+
 # gaussian excitation template object which creates a 2D gaussian surface with a central maximum point
 class ExcitationGaussian1(object):
   def __init__(self,amplitude = 1, beam_centre_x = 0, beam_centre_y = 0, sigma_x = 1, sigma_y = 1, out_col_name='z', column_titles=['x','y','z'], fileName='template.dat'):
@@ -58,7 +60,7 @@ class ExcitationGaussian1(object):
         #print(x_col[idx_row])
         FILE.write("%15.6E\t%15.6E" % (x_col[idx_row], y_col[idx_row]))
         for idx_col in range(len(self.column_titles)-2):
-          if self.column_titles[idx_col+2] == self.out_col_name:
+          if self.column_titles[idx_col+2].lower() in [ x.lower() for x in self.out_col_name ]:
             FILE.write("\t%15.6E" % out_col[idx_row])
           else:
             FILE.write("\t%15.6E" % 0)
@@ -118,7 +120,68 @@ class ExcitationGaussian2(object):
         #print(x_col[idx_row])
         FILE.write("%15.6E\t%15.6E" % (x_col[idx_row], y_col[idx_row]))
         for idx_col in range(len(self.column_titles)-2):
-          if self.column_titles[idx_col+2] == self.out_col_name:
+          if self.column_titles[idx_col+2].lower() in [ x.lower() for x in self.out_col_name ]:
+            FILE.write("\t%15.6E" % out_col[idx_row])
+          else:
+            FILE.write("\t%15.6E" % 0)
+        FILE.write('\n')
+
+    return
+
+# gaussian excitation template object which creates a 2D gaussian surface with a circular maximum of radius c
+class ExcitationUniform(object):
+  def __init__(self,
+   amplitude = None,
+   out_col_name = None,
+   column_titles = None,
+   fileName = None,
+   x_list = None,
+   y_list = None):
+    
+    if amplitude is None: amplitude = 1
+    if out_col_name is None: out_col_name = 'z'
+    if column_titles is None: column_titles = ['x','y','z']
+    if fileName is None: fileName = 'template.dat'
+    if x_list is None: x_list = [-1,0,1]
+    if y_list is None: y_list = [-1,0,1]
+    
+    self.amplitude = amplitude
+    self.out_col_name = out_col_name
+    self.column_titles = column_titles
+    self.fileName = fileName
+    self.x_list = x_list
+    self.y_list = y_list
+
+  def writeDatFile(self,fileName):
+    '''Generate template .dat file for a plane excitation'''
+    #self.x_list = mesh.getXmesh()
+    #self.y_list = mesh.getYmesh()
+    #self.z_list = mesh.getZmesh()
+    
+    x_col = []
+    y_col = []
+    out_col = []
+    
+    print('Generating template N='+str(len(self.x_list)*len(self.y_list)))
+    for x in self.x_list:
+      for y in self.y_list:
+        x_col.append(x)
+        y_col.append(y)
+        out_col.append(self.amplitude)
+    
+    # write file
+    with open(fileName, 'w') as FILE:
+      print('Writing excitation template to '+fileName)
+      for idx_col in range(len(self.column_titles)):
+        if idx_col>0:
+          FILE.write('\t')
+        FILE.write(self.column_titles[idx_col])
+      FILE.write('\n')
+      for idx_row in range(len(out_col)):
+        #print(x_col[idx_row])
+        FILE.write("%15.6E\t%15.6E" % (x_col[idx_row], y_col[idx_row]))
+        for idx_col in range(len(self.column_titles)-2):
+          if self.column_titles[idx_col+2].lower() in [ x.lower() for x in self.out_col_name ]:
             FILE.write("\t%15.6E" % out_col[idx_row])
           else:
             FILE.write("\t%15.6E" % 0)
