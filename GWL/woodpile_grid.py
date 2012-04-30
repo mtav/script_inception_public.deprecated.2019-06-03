67b5ddc26091f861430247a68ffecb6b25b9b3e8
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-04-30 15:50:17 +0100

    -added __init__.py to enable use of things like blender -P testscript.py -added more options to woodpile class to be able to fit Daniel's woodpile model

--- GWL/woodpile_grid.py
+++ GWL/woodpile_grid.py
@@ -79,6 +79,85 @@ def createSubFiles2(DSTDIR):
         subfile_list.append(subfilename)
   return(subfile_list)
 
+def createSubFiles3(DSTDIR):
+  d = 0.3*numpy.sqrt(2)
+  a = d/numpy.sqrt(2) # Distance between two adjacent logs
+  n_logs = 30 # number of logs in each layer
+  w = 0.2*d # width of the logs
+  h = 0.25*d # heigth of logs (should be 1/4 for fcc to not overlap)
+  L = (n_logs-1)*a+w+a # Length of logs (should > (n_logs-1)*a+w)
+  n_layers = 4*7 # Number of layers of logs required
+  
+  XL = 0 # Lower edge of the simulation domain in x direction.
+  YL = 0 # Lower edge of the simulation domain in y direction.
+  ZL = 0 # Lower edge of the simulation domain in z direction.
+
+  XU = 10 # Upper edge of the simulation domain in x direction.
+  YU = 10 # Upper edge of the simulation domain in y direction.
+  ZU = 4 # Upper edge of the simulation domain in z direction.
+  
+  Nlayers_Z = n_layers
+  NRodsPerLayer_X = n_logs
+  NRodsPerLayer_Y = n_logs
+  nVert = 3
+  
+  woodpile_obj = Woodpile()
+  woodpile_obj.BottomToTop = 0
+  woodpile_obj.Nlayers_Z = Nlayers_Z
+  woodpile_obj.NRodsPerLayer_X = NRodsPerLayer_X
+  woodpile_obj.NRodsPerLayer_Y = NRodsPerLayer_Y
+  woodpile_obj.interRodDistance = a
+  woodpile_obj.interLayerDistance = h
+
+  woodpile_obj.LineDistance_Horizontal = 0.050
+  #woodpile_obj.LineDistance_Vertical = 0.100
+  #woodpile_obj.LineDistance_Vertical = 0.050
+  woodpile_obj.LineNumber_Vertical = nVert #int(woodpile_obj.interLayerDistance/woodpile_obj.LineDistance_Vertical)
+  woodpile_obj.LineDistance_Vertical = woodpile_obj.interLayerDistance/woodpile_obj.LineNumber_Vertical
+  woodpile_obj.LineNumber_Horizontal = 1
+
+  woodpile_obj.initialDirection = 0
+
+  woodpile_obj.initialLayerType_X = 0
+  woodpile_obj.initialLayerType_Y = 0
+
+  box_size = XU-XL
+  wall_thickness = 0.5*(box_size-L)
+
+  woodpile_obj.Xmin = wall_thickness
+  woodpile_obj.Xmax = box_size-wall_thickness
+  woodpile_obj.Ymin = wall_thickness
+  woodpile_obj.Ymax = box_size-wall_thickness
+
+  woodpile_obj.Xoffset = 0.5*(w + woodpile_obj.interRodDistance)
+  woodpile_obj.Yoffset = 0.5*(w + woodpile_obj.interRodDistance)
+
+  #woodpile_obj.Xmin = -0.5*L
+  #woodpile_obj.Xmax = 0.5*L
+  #woodpile_obj.Ymin = -0.5*L
+  #woodpile_obj.Ymax = 0.5*L
+
+  woodpile_obj.isSymmetrical = False
+
+  #woodpile_obj.adaptXYMinMax()
+  #subfilename = 'woodpile.Lambda_'+str(Lambda)+'.a_'+str(a)+'.NX_'+str(NRodsPerLayer_X)+'.NY_'+str(NRodsPerLayer_Y)+'.Nlayers_Z_'+str(Nlayers_Z)+'.dVert_'+str(woodpile_obj.LineDistance_Vertical)+'.dHori_'+str(woodpile_obj.LineDistance_Horizontal)+'.gwl'
+  #subfilename = 'woodpile.interRodDist_'+str(woodpile_obj.interRodDistance)+'.interLayerDist_'+str(woodpile_obj.interLayerDistance)+'.NX_'+str(NRodsPerLayer_X)+'.NY_'+str(NRodsPerLayer_Y)+'.Nlayers_Z_'+str(Nlayers_Z)+'.dVert_'+str(woodpile_obj.LineDistance_Vertical)+'.dHori_'+str(woodpile_obj.LineDistance_Horizontal)+'.gwl'
+  #subfilename = 'woodpile.interRodDist_'+str(woodpile_obj.interRodDistance)+'.NX_'+str(NRodsPerLayer_X)+'.NY_'+str(NRodsPerLayer_Y)+'.Nlayers_Z_'+str(Nlayers_Z)+'.dVert_'+str(woodpile_obj.LineDistance_Vertical)+'.dHori_'+str(woodpile_obj.LineDistance_Horizontal)+'.gwl'
+  #subfilename = 'woodpile.interRodDist_'+str(woodpile_obj.interRodDistance)+'.NX_'+str(NRodsPerLayer_X)+'.NY_'+str(NRodsPerLayer_Y)+'.Nlayers_Z_'+str(Nlayers_Z)+'.dVert_'+str(woodpile_obj.LineDistance_Vertical)+'.gwl'
+  subfilename = 'woodpile.interRodDist_'+str(woodpile_obj.interRodDistance)+'.NX_'+str(NRodsPerLayer_X)+'.NY_'+str(NRodsPerLayer_Y)+'.Nlayers_Z_'+str(Nlayers_Z)+'.nVert_'+str(woodpile_obj.LineNumber_Vertical)+'.gwl'
+
+
+
+  filename = DSTDIR + os.path.sep + subfilename
+  GWL_obj = woodpile_obj.getGWL()
+  #(Pmin, Pmax) = GWL_obj.getLimits()
+  GWL_obj.write_GWL(filename, writingOffset = [0,0,0.515 + 0.5*h,0] )
+  #GWL_obj.write_GWL(filename, writingOffset = [L+0.458,0,0.515 + 0.5*h,0] )
+
+  #woodpile_obj.write_GWL(filename)
+
+  return
+
 def single_woodpile(DSTDIR, NX, NY, NZ, interRodDistance, LaserPower, ScanSpeed, BottomToTop):
 ##        os.chdir(DSTDIR)
 
@@ -254,7 +333,8 @@ if __name__ == "__main__":
 
     # Alex woodpiles
     #subfile_list = createSubFiles(DSTDIR)
-    subfile_list = createSubFiles2(DSTDIR)
+    #subfile_list = createSubFiles2(DSTDIR)
+    createSubFiles3(DSTDIR)
     #mainfile_list = []
     #for subfile in subfile_list:
       #mainfilename = 'main_'+subfile
