ce1574010b3bb7c5679b80ae28d0766b7a3ecf44
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-07-25 15:55:07 +0100

    ellipsoids OK for all 6 elliptical cylinder cases

--- bfdtd/ellipsoid.py
+++ bfdtd/ellipsoid.py
@@ -15,6 +15,8 @@ class Ellipsoid(Block):
     self.name = name
     self.layer = layer
     self.group = group
+    self.block_direction = block_direction
+    self.non_elliptical_directions = non_elliptical_directions
     
     # mesh used to discretize the ellipsoid into blocks
     self.mesh = MeshObject()
@@ -33,27 +35,74 @@ class Ellipsoid(Block):
     C = self.getCentro()
     L = self.lower
     size = self.getSize()
-    centre_x = self.mesh.getYmeshCentres()
+    centre_x = self.mesh.getXmeshCentres()
     centre_y = self.mesh.getYmeshCentres()
     centre_z = self.mesh.getZmeshCentres()
-    delta_x = size[0]
+    delta_x = self.mesh.getXmeshDelta()
     delta_y = self.mesh.getYmeshDelta()
     delta_z = self.mesh.getZmeshDelta()
     a = 0.5*size[0]
     b = 0.5*size[1]
     c = 0.5*size[2]
-    for j in range(len(centre_y)):
-      for k in range(len(centre_z)):
-        
+    
+    if self.block_direction=='x' and self.non_elliptical_directions==[False, False, True]:
+      for j in range(len(centre_y)):
         y = -b+centre_y[j]
-        #print(1-pow((y/b),2))
         dimX = 2*a*numpy.sqrt(1-pow((y/b),2))
-        
         block = Block()
-        block.name = "%s.j%d.k%d" % (self.name,j,k)
-        block.setCentro([C[0],L[1]+centre_y[j],L[2]+centre_z[k]])
-        block.setSize([dimX, delta_y[j], delta_z[k]])
+        block.name = "%s.Zx.j%d" % (self.name,j)
+        block.setCentro([C[0],L[1]+centre_y[j],C[2]])
+        block.setSize([dimX, delta_y[j], size[2]])
         block.write_entry(FILE)
+    elif self.block_direction=='y' and self.non_elliptical_directions==[False, False, True]:
+      for i in range(len(centre_x)):
+        x = -a+centre_x[i]
+        dimY = 2*b*numpy.sqrt(1-pow((x/a),2))
+        block = Block()
+        block.name = "%s.Zy.i%d" % (self.name,i)
+        block.setCentro([L[0]+centre_x[i],C[1],C[2]])
+        block.setSize([delta_x[i], dimY, size[2]])
+        block.write_entry(FILE)
+    elif self.block_direction=='x' and self.non_elliptical_directions==[False, True, False]:
+      for k in range(len(centre_z)):
+        z = -c+centre_z[k]
+        dimX = 2*a*numpy.sqrt(1-pow((z/c),2))
+        block = Block()
+        block.name = "%s.Yx.k%d" % (self.name,k)
+        block.setCentro([C[0],C[1],L[2]+centre_z[k]])
+        block.setSize([dimX, size[1], delta_z[k]])
+        block.write_entry(FILE)
+    elif self.block_direction=='z' and self.non_elliptical_directions==[False, True, False]:
+      for i in range(len(centre_x)):
+        x = -a+centre_x[i]
+        dimZ = 2*c*numpy.sqrt(1-pow((x/a),2))
+        block = Block()
+        block.name = "%s.Yz.i%d" % (self.name,i)
+        block.setCentro([L[0]+centre_x[i],C[1],C[2]])
+        block.setSize([delta_x[i], size[1], dimZ])
+        block.write_entry(FILE)
+    elif self.block_direction=='y' and self.non_elliptical_directions==[True, False, False]:
+      for k in range(len(centre_z)):
+        z = -c+centre_z[k]
+        dimY = 2*b*numpy.sqrt(1-pow((z/c),2))
+        block = Block()
+        block.name = "%s.Xy.k%d" % (self.name,k)
+        block.setCentro([C[0],C[1],L[2]+centre_z[k]])
+        block.setSize([size[0], dimY, delta_z[k]])
+        block.write_entry(FILE)
+    elif self.block_direction=='z' and self.non_elliptical_directions==[True, False, False]:
+      for j in range(len(centre_y)):
+        y = -b+centre_y[j]
+        dimZ = 2*c*numpy.sqrt(1-pow((y/b),2))
+        block = Block()
+        block.name = "%s.Xz.j%d" % (self.name,j)
+        block.setCentro([C[0],L[1]+centre_y[j],C[2]])
+        block.setSize([size[0], delta_y[j], dimZ])
+        block.write_entry(FILE)
+    else:
+      print('FATAL ERROR: not supported yet', file=sys.stderr)
+      sys.exit(-1)
+      
     return
 
 # for testing
@@ -70,6 +119,23 @@ if __name__ == "__main__":
   ellipsoid.setCentro(block.getCentro())
   ellipsoid.setSize(block.getSize())
   ellipsoid.mesh.setSizeAndResolution(ellipsoid.getSize(),[10,10,10])
+
   sim.geometry_object_list.append(ellipsoid)
-  
-  sim.writeGeoFile(sys.argv[1])
+
+  ellipsoid.non_elliptical_directions = [True, False, False]
+  ellipsoid.block_direction = 'y'
+  sim.writeGeoFile('ellipsoid.Xy.geo')
+  ellipsoid.block_direction = 'z'
+  sim.writeGeoFile('ellipsoid.Xz.geo')
+
+  ellipsoid.non_elliptical_directions = [False, True, False]
+  ellipsoid.block_direction = 'z'
+  sim.writeGeoFile('ellipsoid.Yz.geo')
+  ellipsoid.block_direction = 'x'
+  sim.writeGeoFile('ellipsoid.Yx.geo')
+
+  ellipsoid.non_elliptical_directions = [False, False, True]
+  ellipsoid.block_direction = 'x'
+  sim.writeGeoFile('ellipsoid.Zx.geo')
+  ellipsoid.block_direction = 'y'
+  sim.writeGeoFile('ellipsoid.Zy.geo')
