311a3c4460a6e32684565ac02a68007fae1cc7ac
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-08-08 12:02:07 +0100

    added snap_time_number as input argument to findPrnByName

--- utilities/findPrnByName.m
+++ utilities/findPrnByName.m
@@ -1,4 +1,7 @@
-function PrnFileNameList = findPrnByName(inputFileList,name)
+function PrnFileNameList = findPrnByName(inputFileList,name,snap_time_number)
+
+  if exist('snap_time_number','var')==0; snap_time_number = 0; end
+
   PrnFileNameList = {};
   [entries,FDTDobj] = GEO_INP_reader(inputFileList);
   for numID = 1:length(FDTDobj.probe_list)
@@ -17,7 +20,7 @@ function PrnFileNameList = findPrnByName(inputFileList,name)
       snap_plane_list = {'x','y','z'};
       snap_plane = snap_plane_list{FDTDobj.frequency_snapshots(numID).plane};
       probe_ident = FDTDobj.flag.id;
-      [ filename, alphaID, pair ] = numID_to_alphaID(numID, snap_plane, probe_ident);
+      [ filename, alphaID, pair ] = numID_to_alphaID(numID, snap_plane, probe_ident, snap_time_number);
       %num = sprintf('%03d',numID);
       %filename = strcat({snap_plane}, num, {probe_ident}, '.prn');
       %filename = char(filename);
