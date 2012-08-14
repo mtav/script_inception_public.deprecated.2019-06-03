9e6ca98a8efdb00170067bd80512a542bf672587
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-08-14 16:02:54 +0100

    ID listers successfully tested

--- ID_listers/ID_test.py
+++ ID_listers/ID_test.py
@@ -9,14 +9,30 @@ f = Frequency_snapshot()
 sim = BFDTDobject()
 sim.flag.iterations=10
 
+sim.fileList = []
 sim.probe_list = 439*[p]
 sim.snapshot_list = []
 sim.writeAll('Probes')
 
+sim.fileList = []
 sim.probe_list = []
 sim.snapshot_list = 439*[t]
 sim.writeAll('TimeSnapshots')
 
+sim.fileList = []
 sim.probe_list = []
 sim.snapshot_list = 836*[f]
 sim.writeAll('FrequencySnapshots')
+
+# TESTING COMMANDS:
+#grep -ci ^probe */*.inp
+#grep -ci ^snapshot */*.inp
+#grep -ci ^frequency_snapshot */*.inp
+
+#ls -1 Probes/*.prn | wc -l
+#ls -1 TimeSnapshots/*.prn | wc -l
+#ls -1 FrequencySnapshots/*.prn | wc -l
+
+#./FrequencySnapshot_IDs | xargs -n1 -I{} ls FrequencySnapshots/{} 1>/dev/null
+#./TimeSnapshot_IDs | xargs -n1 -I{} ls TimeSnapshots/{} 1>/dev/null
+#./probe_IDs | xargs -n1 -I{} ls Probes/{} 1>/dev/null
