c2fa33f4461ed16d8478a518bf3b90ca7707cfcb
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-11-07 15:56:27 +0000

    WIP on qstat output parser

--- bin/superqstat.py
+++ bin/superqstat.py
@@ -7,84 +7,40 @@ import re
 
 def parseFile(infile):
 
-  #pattern = r""
-  #Job Id: (.*)
-      #Job_Name = (.*)
-      #Job_Owner = (.*)
-      #job_state = (.*)
-      #queue = (.*)
-      #server = (.*)
-      #Checkpoint = (.*)
-      #ctime = (.*)
-      #Error_Path = (.*)
-    #/qsub.chr.9.22.e1600362
-      #Hold_Types = (.*)
-      #Join_Path = (.*)
-      #Keep_Files = (.*)
-      #Mail_Points = (.*)
-      #mtime = (.*)
-      #Output_Path = (.*)
-    #s/qsub.chr.9.22.o1600362
-      #Priority = (.*)
-      #qtime = (.*)
-      #Rerunable = (.*)
-      #Resource_List.nodect = (.*)
-      #Resource_List.nodes = (.*)
-      #Resource_List.walltime = (.*)
-      #etime = (.*)
-      #submit_args = (.*)
-
   QFILE = infile
   with open(QFILE, 'r') as f:
     # read the whole file as one string
     fulltext = f.read()
+
+  pattern_job = re.compile("Job Id: (?P<jobId>.*?)\n(?P<jobDetails>.*?)\n\n",re.DOTALL)
+
+  job_blob_iterator = pattern_job.finditer(fulltext)
+  Njobs = sum(1 for _ in job_blob_iterator)
+  print('Number of jobs = '+str(Njobs))
   
-  print(fulltext)
+  job_blob_iterator = pattern_job.finditer(fulltext)
+  job_blob = job_blob_iterator.__next__()
+
+  #print(job_blob.groupdict())
+  print(job_blob.groupdict()['jobId'])
+  job_details = job_blob.groupdict()['jobDetails']
+  #print(job_details)
   
-  #pattern_objects = re.compile("(?P<Type>\w+)\s*(?P<nameblob>[^{}]+)?{(?P<data>[^{}]*)}",re.DOTALL)
-  pattern_objects = re.compile("Job Id: (.*)\n\n",re.DOTALL)
-  objects = [m.groupdict() for m in pattern_objects.finditer(fulltext)]
+  job_details = job_details.replace('\n\t','')
+  #print(job_details)
 
-  #entries = []
-  ## process objects
-  #for i in range(len(objects)):
-      #Type = objects[i]['Type']
-      #name = ''
-      #if 'nameblob' in objects[i].keys():
-        ##print objects[i]['nameblob']
-        #if objects[i]['nameblob']:
-          ##print 'OK'
-          #pattern_nameblob = re.compile("\*\*name=(.*)")
-          #m = pattern_nameblob.match(objects[i]['nameblob'])
-          #if m:
-            #name = m.group(1).strip()
-        ##else:
-          ##print 'NOT OK'
-          ##name = ''
-      ##else:
-        ##print 'NO NAME'
-        ##name = ''
-      #data = objects[i]['data']
-      
-      ## convert Type to upper case and strip it
-      #Type = Type.upper().strip()
-      ## split data by spaces and new lines
-      #data = re.split('\s+',data)
-      ## remove empty lines from data
-      ##data = filter(None, data)
-      #data = list(filter(None, data))
-      ##print('data = '+str(data))
-      
-      #entry = Entry()
-      #entry.Type = Type
-      #entry.name = name
-      #entry.data = data
-      #entries.append(entry)
+  pattern_jobitems = re.compile("\s*(?P<key>.*)\s*=\s*(?P<value>.*)\s*")
+  job_items_iterator = pattern_jobitems.finditer(job_details)
 
-  #for line in f:
-    #if len(line.strip())>0:
-      #tab = line.strip().split('\t')
-
+  #job_item = job_items_iterator.__next__()
+  #print(job_item.groupdict())
+  
+  for job_item in job_items_iterator:
+    print(job_item.groupdict())
+  
+  #for job_blob in job_blob_iterator:
+    #print(job_blob)
+  
   return
 
 def main():
