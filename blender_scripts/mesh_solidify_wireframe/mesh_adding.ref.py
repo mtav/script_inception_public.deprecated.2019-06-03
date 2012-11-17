286b3ae6d9d18b80a533fc4ca6a1723ac894fc87
Mike Taverne <Mike.Taverne@bristol.ac.uk>
2012-11-17 10:38:54 +0000

    -adding small blender scripting reference -adding temporary rsync hack script

---
+++ blender_scripts/mesh_solidify_wireframe/mesh_adding.ref.py
@@ -0,0 +1,6 @@
+mesh_data = bpy.data.meshes.new('name')
+mesh_data.from_pydata([(0,0,0),(1,0,0),(1,1,0),(0,1,0)], [(0,1),(1,2),(2,3),(3,0)], [(0,1,2,3)])
+new_object = bpy.data.objects.new('name', mesh_data)
+scene = bpy.context.scene
+scene.objects.link(new_object)
+
